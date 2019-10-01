cce12d5c77d5fc0922ae3a6e37273e4458c35176
mrkwon <erased11@gmail.com>
2019-10-01 16:19:27 +0900

    feat: 요구사항 1 - 애노테이션 기반 MVC 프레임워크

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,21 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import nextstep.web.support.MediaType;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.logging.Handler;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,10 +27,21 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
 
+        controllers.stream()
+                .flatMap(controller -> Arrays.stream(controller.getMethods()))
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> {
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    HandlerKey handlerKey = new HandlerKey(annotation.value(), annotation.method());
+                    handlerExecutions.put(handlerKey, new HandlerExecution(method));
+                });
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.resolve(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- nextstep-mvc/src/main/java/nextstep/web/annotation/RequestMethod.java
+++ nextstep-mvc/src/main/java/nextstep/web/annotation/RequestMethod.java
@@ -1,5 +1,14 @@
 package nextstep.web.annotation;
 
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod resolve(String method) {
+        for (RequestMethod requestMethod : RequestMethod.values()) {
+            if (requestMethod.name().equals(method.toUpperCase())) {
+                return requestMethod;
+            }
+        }
+        throw new IllegalArgumentException("지원하지 않는 메서드입니다.");
+    }
 }
