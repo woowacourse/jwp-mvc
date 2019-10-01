5a99d09b913f16f8648f0c05a7e7b8683b448126
mrkwon <erased11@gmail.com>
2019-10-01 14:28:22 +0900

    feat: 요구사항 3 @Test 애노테이션 메소드 실행 완료

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,12 +1,25 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class Junit4TestRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Junit4TestRunner.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            log.info("method : {}", method);
+            if (method.isAnnotationPresent(MyTest.class)) {
+                log.info("annotation : {}", method.getAnnotation(MyTest.class));
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
