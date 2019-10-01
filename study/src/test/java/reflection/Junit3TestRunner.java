7b898e664ef5de21580416fe32fae171cbd3651e
Seongmo Jeon <oeeen3@gmail.com>
2019-10-01 14:09:03 +0900

    feat: 요구사항 2 - test로 시작하는 메소드 실행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,11 +2,25 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            if (getMethodName(method).startsWith("test")) {
+                method.invoke(clazz.newInstance());
+            }
+        }
+    }
+
+    private String getMethodName(Method method) {
+        String[] tokens = method.toGenericString().split(" ");
+        String[] structures = tokens[2].split("\\.");
+        return structures[2];
     }
 }
