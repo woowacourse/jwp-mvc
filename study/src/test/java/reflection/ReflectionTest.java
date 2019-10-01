1cc3917b379aaa94cd46fb86e2e4496f07eee61c
Seongmo Jeon <oeeen3@gmail.com>
2019-10-01 14:58:33 +0900

    feat: 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성

--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Date;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -39,15 +40,21 @@ public class ReflectionTest {
     public void constructor_with_args() throws Exception {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
+        Object[] parameters1 = {"a", "b", "c"};
+        Object[] parameters2 = {1L, "a", "b", "c", new Date(), 1};
+
         for (Constructor constructor : constructors) {
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+            Question question = null;
+            if (parameterTypes.length == 3) {
+                question = (Question) constructor.newInstance(parameters1);
             }
-        }
 
-        // TODO 인자를 가진 생성자를 활용해 인스턴스를 생성한다.
+            if (parameterTypes.length == 6) {
+                question = (Question) constructor.newInstance(parameters2);
+            }
+            logger.debug(question.toString());
+        }
     }
 
     @Test
