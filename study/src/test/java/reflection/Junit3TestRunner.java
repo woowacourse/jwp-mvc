1d5ea347f9c6cfdc6bee23f058942d08f626466e
school0bhy <house1217@ajou.ac.kr>
2019-10-01 15:57:03 +0900

    Java Reflection 실습: 요구사항2 - test로 시작하는 메소드 실행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,11 +2,30 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(this::startsWithTest)
+                .forEach(method -> invoke(clazz, method));
+    }
+
+    private void invoke(Class<Junit3Test> clazz, Method method) {
+        try {
+            method.invoke(clazz.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean startsWithTest(Method method) {
+        return method.getName().startsWith("test");
     }
 }
