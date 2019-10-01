bd40baa590e6a7566f6edbcae71c551278a9a7ca
hyogeon <men1219@hanmail.net>
2019-10-01 16:40:37 +0900

    feat: Annotation 기반 Request Handling

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,16 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,10 +22,27 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
 
+        controllers.forEach(this::fillHandlerExecutions);
+    }
+
+    private void fillHandlerExecutions(Class<?> aClass) {
+        Arrays.stream(aClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(this::fillHandlerExecution);
+    }
+
+    private void fillHandlerExecution(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMapping.method()), new HandlerExecution(method));
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        String url = request.getRequestURI();
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(url, RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
