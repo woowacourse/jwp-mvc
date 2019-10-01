59966844a4532c5084bbc1ffafc70e25dbec25ff
wbluke <pwb0127@naver.com>
2019-10-01 17:09:10 +0900

    feat: 어노테이션을 기반으로 한 MVC 프레임워크 구현

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,15 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,10 +21,39 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerClazz = reflections.getTypesAnnotatedWith(Controller.class);
 
+        for (Class<?> clazz : controllerClazz) {
+            appendHandlerExecutions(clazz);
+        }
+    }
+
+    private void appendHandlerExecutions(final Class<?> clazz) {
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                HandlerKey handlerKey = createHandlerKey(method);
+
+                HandlerExecution handlerExecution
+                        = (request, response) -> (ModelAndView) method.invoke(clazz.newInstance(), request, response);
+                handlerExecutions.put(handlerKey, handlerExecution);
+            }
+        }
+    }
+
+    private HandlerKey createHandlerKey(final Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        String url = annotation.value();
+        RequestMethod requestMethod = annotation.method();
+        return new HandlerKey(url, requestMethod);
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
