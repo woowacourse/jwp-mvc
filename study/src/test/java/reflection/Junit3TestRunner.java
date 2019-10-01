a8dadf1518e3b9e7f4c7c2f75f249d56a1b48255
soojinroh <rsj3112@gmail.com>
2019-10-01 14:27:04 +0900

    feat : Junit4TestRunner 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,29 +1,17 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
-public class Junit3TestRunner {
-    private static final Logger logger = LoggerFactory.getLogger(Junit3TestRunner.class);
-
+public class Junit3TestRunner extends AbstractTestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
         Junit3Test test = clazz.getConstructor().newInstance();
 
         Arrays.stream(clazz.getDeclaredMethods())
                 .filter(method -> method.getName().startsWith("test"))
-                .forEach(method -> {
-                    try {
-                        method.invoke(test);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        logger.debug(e.getMessage());
-                    }
-                });
+                .forEach(method -> invoke(method, test));
     }
 }
