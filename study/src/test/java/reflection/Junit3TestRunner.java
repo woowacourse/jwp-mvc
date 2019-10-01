fe4b036b7b81551454a41d50d7d4bce163d02e01
Jo Jinsik <kunggom@gmail.com>
2019-10-01 16:56:26 +0900

    feat(Junit3TestRunner): Reflection 실습 요구사항 2 달성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,11 +2,21 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
-public class Junit3TestRunner {
+import java.lang.reflect.Method;
+
+class Junit3TestRunner {
+    private static final String TEST = "test";
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // Junit3Test에서 test로 시작하는 메소드 실행
+        for (final Method method : clazz.getMethods()) {
+            final String methodName = method.getName();
+            if (methodName.startsWith(TEST)) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
