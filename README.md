44d8ee36a7bb44ab3be202df59bcf66eaeaf92b2
heebong <heebg2@naver.com>
2019-10-01 14:18:17 +0900

    test: reflection method study

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ https://gist.github.com/joshbuchea/6f47e86d2510bce28f8e7f42ae84c716
 
 ## TODO
 #### 요구사항 1
-- [ ] study 모듈 > src/test/java > reflection > ReflectionTest의 showClass() 메소드를 구현해 Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+- [x] study 모듈 > src/test/java > reflection > ReflectionTest의 showClass() 메소드를 구현해 Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
 - [x] Question 클래스의 모든 필드 출력
 - [x] Question 클래스 생성자 출력
-- [ ] Question 클래스 모든 메소드 출력
+- [x] Question 클래스 모든 메소드 출력
--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -15,12 +15,20 @@ public class ReflectionTest {
     public void showClass() throws NoSuchMethodException {
         Class<Question> clazz = Question.class;
         logger.debug("클래스 이름 : {} ", clazz.getName());
+
         Field[] fields = clazz.getDeclaredFields();
         Arrays.stream(fields)
             .forEach(field -> logger.debug("필드 이름 : {}, 필드 타입 : {}", field.getName(), field.getType()));
+
         Arrays.stream(clazz.getConstructors())
             .forEach(constructor -> logger.debug("생성자 이름: {} 생성자: {}", constructor.getName(), constructor));
         logger.debug(clazz.getConstructor(String.class, String.class, String.class).toString());
+
+        Arrays.stream(clazz.getMethods())
+            .forEach(method -> logger.debug("메소드 이름: {}, 파라미터: {}, 리턴 타입: {}", method.getName(), method.getParameterCount(), method.getReturnType()));
+
+        Arrays.stream(clazz.getDeclaredMethods())
+            .forEach(method -> logger.debug("메소드 이름: {}, 파라미터: {}, 리턴 타입: {}", method.getName(), method.getParameterCount(), method.getReturnType()));
         // TODO Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
     }
 
