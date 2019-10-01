a708fe16c6b352d25a27ac39acf97e16fbf86b7d
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-10-01 15:52:59 +0900

    feat: learning test2 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,12 +1,30 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class Junit3TestRunner {
+    private static final Logger log = LoggerFactory.getLogger(Junit3TestRunner.class);
+
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // Junit3Test에서 test로 시작하는 메소드 실행
+
+        Object object = clazz.getDeclaredConstructor().newInstance();
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(object);
+                    } catch (Exception e) {
+                        System.out.println(e.getCause());
+                    }
+                });
+
     }
 }
