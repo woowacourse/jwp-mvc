4536dffceaba1d9bd399c43d693b143352ffef44
김성훈 <seonghun127@gmail.com>
2019-10-01 20:03:56 +0900

    feat: implement HandlerMapping

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,16 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,10 +22,37 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflection = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflection.getTypesAnnotatedWith(Controller.class);
 
+        for (Class<?> controller : controllers) {
+            Method[] methods = controller.getDeclaredMethods();
+            for (Method method : methods) {
+                putHandlerExecution(controller, method);
+            }
+        }
+    }
+
+    private void putHandlerExecution(Class<?> controller, Method method) {
+        if (method.isAnnotationPresent(RequestMapping.class)) {
+            String url = method.getAnnotation(RequestMapping.class).value();
+            RequestMethod requestMethod = method.getAnnotation(RequestMapping.class).method();
+            if (Objects.isNull(requestMethod)) {
+                for (RequestMethod value : RequestMethod.values()) {
+                    HandlerKey handlerKey = new HandlerKey(url,value);
+                    handlerExecutions.put(handlerKey,
+                            (request, response) -> (ModelAndView) method.invoke(controller.newInstance(), request, response));
+                }
+                return;
+            }
+            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(handlerKey,
+                    (request, response) -> (ModelAndView) method.invoke(controller.newInstance(), request, response));
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.of(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- nextstep-mvc/src/main/java/nextstep/web/annotation/RequestMethod.java
+++ nextstep-mvc/src/main/java/nextstep/web/annotation/RequestMethod.java
@@ -1,5 +1,31 @@
 package nextstep.web.annotation;
 
+import nextstep.mvc.exception.NotFoundHttpMethodException;
+
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET("GET"),
+    HEAD("HEAD"),
+    POST("POST"),
+    PUT("PUT"),
+    PATCH("PATCH"),
+    DELETE("DELETE"),
+    OPTIONS("OPTIONS"),
+    TRACE("TRACE");
+
+
+    private final String method;
+
+    RequestMethod(String method) {
+        this.method = method;
+    }
+
+    public static RequestMethod of(String methodType) {
+        return Arrays
+                .stream(values())
+                .filter(m -> methodType.equals(m.method))
+                .findFirst()
+                .orElseThrow(NotFoundHttpMethodException::new);
+    }
 }
