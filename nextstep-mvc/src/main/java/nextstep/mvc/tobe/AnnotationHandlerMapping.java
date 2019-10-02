d6a04b41e6f91a210142439d4eb1f8b8272c5e63
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-10-02 11:21:53 +0900

    feat: Implement Annotation Handler Mapping

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,25 +1,56 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+    private Object[] basePackages;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
-    public AnnotationHandlerMapping(Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(Object... basePackages) {
+        this.basePackages = basePackages;
     }
 
     public void initialize() {
+        checkBasePackages();
+    }
 
+    private void checkBasePackages() {
+        for (Object basePackage : basePackages) {
+            Reflections reflections = new Reflections(basePackage);
+            checkClazz(reflections);
+        }
+    }
+
+    private void checkClazz(Reflections reflections) {
+        for (Class<?> clazz : reflections.getTypesAnnotatedWith(Controller.class)) {
+            checkMethods(clazz);
+        }
+    }
+
+    private void checkMethods(Class<?> clazz) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            putHandlerExecution(method);
+        }
+    }
+
+    private void putHandlerExecution(Method method) {
+        if (method.isAnnotationPresent(RequestMapping.class)) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMapping.method()), new HandlerExecution(method));
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        return handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.of(request.getMethod())));
     }
 }
