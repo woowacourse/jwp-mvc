56bffeb8ca4d1d9884bebd5df7f981539dbf30ef
kangmin46 <kangmin789@naver.com>
2019-10-02 13:50:50 +0900

    [chore] reorganize package structure

--- slipp/src/main/java/slipp/asis/controller/ListUserController.java
+++ slipp/src/main/java/slipp/asis/controller/ListUserController.java
@@ -1,11 +1,15 @@
 package slipp.controller;
 
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
+import org.springframework.http.HttpMethod;
 import slipp.support.db.DataBase;
 import nextstep.mvc.asis.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@RequestMapping(value = "/users", method = RequestMethod.GET)
 public class ListUserController implements Controller {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
