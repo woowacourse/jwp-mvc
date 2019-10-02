6b1e7440b0af71dade6ac76b9d8858bf67d0067c
MK S <conatuseus@gmail.com>
2019-10-02 12:59:21 +0900

    [feat] java.lang.Class를 사용해 클래스 정보 확인

--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +15,26 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug("getModifier: {} --- {}", clazz.getModifiers(), Modifier.toString(clazz.getModifiers()));
+        logger.debug("\n");
+
+        logger.debug("getFields: {}", Arrays.toString(clazz.getFields()));
+        logger.debug("\n");
+
+        logger.debug("getDeclaredFields: {}", Arrays.toString(clazz.getDeclaredFields()));
+        logger.debug("\n");
+
+        logger.debug("getConstructors: {}", Arrays.toString(clazz.getConstructors()));
+        logger.debug("\n");
+
+        logger.debug("getDeclaredConstructors: {}", Arrays.toString(clazz.getDeclaredConstructors()));
+        logger.debug("\n");
+
+        logger.debug("getMethods: {}", Arrays.toString(clazz.getMethods()));
+        logger.debug("\n");
+
+        logger.debug("getDeclaredMethods: {}", Arrays.toString(clazz.getDeclaredMethods()));
 
         // TODO Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
     }
