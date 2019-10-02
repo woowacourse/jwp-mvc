baed56f9bee0cee1666cb65d660c694500456d8f
backlo <hjun4512@naver.com>
2019-10-02 11:06:23 +0900

    feat: Reflection 을 사용해 test 로 시작하는 메소드를 동적으로 실행함

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,12 +1,29 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class Junit3TestRunner {
+    private static final Logger log = LoggerFactory.getLogger(Junit3TestRunner.class);
+
     @Test
-    public void run() throws Exception {
+    public void run() {
+        Junit3Test target = new Junit3Test();
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(target);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        log.error("error: {}", e.getMessage());
+                    }
+                });
+
     }
 }
