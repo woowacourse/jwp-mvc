32aac58f2e47eabb81187c7cc7781dcfc2e829b4
soojinroh <rsj3112@gmail.com>
2019-10-02 17:02:05 +0900

    refactor : 클래스명 변경
    HandlerMapping 클래스를 LegacyHandlerMapping으로 이름 변경

--- slipp/src/main/java/slipp/ManualLegacyHandlerMapping.java
+++ slipp/src/main/java/slipp/ManualLegacyHandlerMapping.java
@@ -1,7 +1,7 @@
 package slipp;
 
 import nextstep.mvc.DispatcherServlet;
-import nextstep.mvc.HandlerMapping;
+import nextstep.mvc.LegacyHandlerMapping;
 import nextstep.mvc.asis.Controller;
 import nextstep.mvc.asis.ForwardController;
 import org.slf4j.Logger;
@@ -11,20 +11,17 @@ import slipp.controller.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ManualHandlerMapping implements HandlerMapping {
+public class ManualLegacyHandlerMapping implements LegacyHandlerMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private Map<String, Controller> mappings = new HashMap<>();
 
     @Override
     public void initialize() {
-        mappings.put("/", new HomeController());
         mappings.put("/users/form", new ForwardController("/user/form.jsp"));
         mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
-        mappings.put("/users", new ListUserController());
         mappings.put("/users/login", new LoginController());
         mappings.put("/users/profile", new ProfileController());
         mappings.put("/users/logout", new LogoutController());
-        mappings.put("/users/create", new CreateUserController());
         mappings.put("/users/updateForm", new UpdateFormUserController());
         mappings.put("/users/update", new UpdateUserController());
 
