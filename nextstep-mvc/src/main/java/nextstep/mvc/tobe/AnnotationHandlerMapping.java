d120666e19da7a9d9940dc259915eb575d9c422f
kangmin46 <kangmin789@naver.com>
2019-10-02 13:53:17 +0900

    [feat] add scan method

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,18 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -16,10 +24,47 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        scanControllers(reflections);
+        // TODO scan services
+    }
 
+    private void scanControllers(Reflections reflections) {
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        try {
+            for (Class<?> controller : controllers) {
+                findHandlersInController(controller);
+            }
+        } catch (Exception e) {
+            throw new AnnotationScanFailException(e.getCause());
+        }
+    }
+
+    private void findHandlersInController(Class<?> controller) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Object controllerInstance = controller.getDeclaredConstructor().newInstance();
+        Method[] methods = controller.getDeclaredMethods();
+        Arrays.stream(methods)
+            .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .forEach(method -> addHandlerExcution(controllerInstance, method));
+    }
+
+    private void addHandlerExcution(Object controllerInstance, Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        String url = annotation.value();
+        RequestMethod requestMethod = annotation.method();
+        HandlerKey key = new HandlerKey(url, requestMethod);
+        handlerExecutions.put(key, (req, res) -> (ModelAndView) method.invoke(controllerInstance, req, res));
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        String url = request.getRequestURI();
+        RequestMethod method = RequestMethod.valueOf(request.getMethod());
+        HandlerExecution handler = handlerExecutions.get(new HandlerKey(url, method));
+
+        if(handler == null) {
+            throw new HandlerNotFoundException();
+        }
+
+        return handler;
     }
 }
---
+++ slipp/src/main/java/slipp/tobe/controller/UserController.java
@@ -0,0 +1,39 @@
+package slipp.tobe.controller;
+
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    ModelAndView get(HttpServletRequest req, HttpServletResponse res) {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        return null;
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    ModelAndView post(HttpServletRequest req, HttpServletResponse res) {
+        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+            req.getParameter("email"));
+        logger.debug("User : {}", user);
+
+        DataBase.addUser(user);
+        return null;
+    }
+}
