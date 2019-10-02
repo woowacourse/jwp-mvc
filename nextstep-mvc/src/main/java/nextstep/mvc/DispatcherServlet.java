a61277f1b7f67a577ec70937889e8b876b5efa79
school0bhy <house1217@ajou.ac.kr>
2019-10-02 17:05:41 +0900

    feat: 레거시 MVC와 애노테이션 기반 MVC 통합

--- nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,6 +1,7 @@
 package nextstep.mvc;
 
-import nextstep.mvc.asis.Controller;
+import nextstep.mvc.exception.HandlerNotFoundException;
+import nextstep.mvc.tobe.HandlerExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
@@ -18,15 +21,15 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
-    private HandlerMapping rm;
+    private List<HandlerMapping> requestMappings;
 
-    public DispatcherServlet(HandlerMapping rm) {
-        this.rm = rm;
+    public DispatcherServlet(List<HandlerMapping> requestMappings) {
+        this.requestMappings = requestMappings;
     }
 
     @Override
     public void init() throws ServletException {
-        rm.initialize();
+        requestMappings.forEach(HandlerMapping::initialize);
     }
 
     @Override
@@ -34,14 +37,24 @@ public class DispatcherServlet extends HttpServlet {
         String requestUri = req.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
 
-        Controller controller = rm.getHandler(requestUri);
         try {
-            String viewName = controller.execute(req, resp);
+            String viewName = findHandler(req).execute(req, resp);
             move(viewName, req, resp);
+        } catch (HandlerNotFoundException e) {
+            logger.error("Exception : {}", e);
+            resp.sendError(404);
         } catch (Throwable e) {
             logger.error("Exception : {}", e);
             throw new ServletException(e.getMessage());
         }
+    }
+
+    private HandlerExecution findHandler(HttpServletRequest req) {
+        return requestMappings.stream()
+                .map(requestMapping -> requestMapping.getHandler(req))
+                .filter(Objects::nonNull)
+                .findAny()
+                .orElseThrow(HandlerNotFoundException::new);
     }
 
     private void move(String viewName, HttpServletRequest req, HttpServletResponse resp)
--- nextstep-mvc/src/main/java/nextstep/mvc/asis/Controller.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/asis/Controller.java
@@ -1,8 +1,9 @@
 package nextstep.mvc.asis;
 
+import nextstep.mvc.tobe.HandlerExecution;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface Controller {
-    String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception;
+public interface Controller extends HandlerExecution {
 }
