1296aa384ae575dd2a34d9ec6cca15f854e80955
backlo <hjun4512@naver.com>
2019-10-02 11:11:27 +0900

    test: Reflection 을 통한 @Test 어노테이션 메소드 실행 로직 구현

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,12 +1,27 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class Junit4TestRunner {
+    private static final Logger log = LoggerFactory.getLogger(Junit4TestRunner.class);
+
     @Test
-    public void run() throws Exception {
+    public void run() {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.newInstance());
+                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                        log.error("error: {}", e.getMessage());
+                    }
+                });
     }
 }
