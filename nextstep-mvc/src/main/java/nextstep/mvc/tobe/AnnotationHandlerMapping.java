2d116bec700878022392973e1d324e92f3a98ae8
Seongmo Jeon <oeeen3@gmail.com>
2019-10-02 12:51:20 +0900

    feat: 애노테이션 기반 MVC 프레임워크

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,22 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -16,10 +26,22 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> clazz = reflections.getTypesAnnotatedWith(Controller.class);
+        clazz.stream()
+                .flatMap(c -> Arrays.stream(c.getDeclaredMethods())
+                        .filter(m -> m.isAnnotationPresent(RequestMapping.class)))
+                .forEach(this::createHandlerKey);
+    }
 
+    private void createHandlerKey(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method());
+        handlerExecutions.put(handlerKey, (req, res) -> (ModelAndView) method.invoke(method.getDeclaringClass().newInstance(), req, res));
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
