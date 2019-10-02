0bd7f3c72ea1df74f50ec84b76276ceffa9c9f78
김성훈 <seonghun127@gmail.com>
2019-10-02 16:49:19 +0900

    refactor: change legacy MVC to annotation-based MVC

--- slipp/src/main/java/slipp/controller/UserController.java
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -8,11 +8,13 @@ import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
+
     @RequestMapping(value = "/users/create", method = RequestMethod.POST)
-    public String save(HttpServletRequest req, HttpServletResponse res) throws Exception {
+    public String save(HttpServletRequest req, HttpServletResponse res) {
         User user = new User(
                 req.getParameter("userId"),
                 req.getParameter("password"),
@@ -20,6 +22,32 @@ public class UserController {
                 req.getParameter("email"));
 
         DataBase.addUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/users/login", method = RequestMethod.POST)
+    public String login(HttpServletRequest req, HttpServletResponse res) {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            req.setAttribute("loginFailed", true);
+            return "/user/login.jsp";
+        }
+        if (user.matchPassword(password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
+            return "redirect:/";
+        } else {
+            req.setAttribute("loginFailed", true);
+            return "/user/login.jsp";
+        }
+    }
+
+    @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest req, HttpServletResponse res) {
+        HttpSession session = req.getSession();
+        session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
         return "redirect:/";
     }
 }
