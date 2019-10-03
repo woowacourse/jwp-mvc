c73c95876629da8f8f3fba311177ee6892677104
권민철 <erased11@gmail.com>
2019-10-03 17:36:57 +0900

    [MVC - Step 1] 루피 미션 제출합니다. (#5)
    * docs: 1단계 Java Reflection 실습 요구사항 정리
    
    * feat: 요구사항1 클래스 정보 출력 완료
    
    * feat: 요구사항 2 test로 시작하는 메소드 실행 완료
    
    * feat: 요구사항 3 @Test 애노테이션 메소드 실행 완료
    
    * feat: 요구사항 4 private field에 값 할당
    
    * feat: 요구사항 5 인자를 가진 생성자의 인스턴스 생성
    
    * feat: 요구사항 6 component scan
    
    * docs: 1단계 @MVC 구현 요구사항 정리
    
    * feat: 요구사항 1 - 애노테이션 기반 MVC 프레임워크
    
    * feat: 요구사항 2 - 레거시 MVC와 애노테이션 기반 MVC 통합
    
    * refactor: @RequestMapping 에 method 설정이 되어 있지 않으면 모든 HTTP method 를 지원하도록 수정

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
@@ -2,9 +2,23 @@ package nextstep.mvc.tobe;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
-    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+    private Method method;
+
+    public HandlerExecution(Method method) {
+        this.method = method;
+    }
+
+    public String handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return (String) method.invoke(method.getDeclaringClass().newInstance(), request, response);
+    }
+
+    @Override
+    public String toString() {
+        return "HandlerExecution{" +
+                "method=" + method +
+                '}';
     }
 }
