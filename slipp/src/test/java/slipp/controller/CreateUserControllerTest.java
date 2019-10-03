808fcf4802ab647d2b83c96b89585115b9a1917a
soojinroh <rsj3112@gmail.com>
2019-10-03 17:10:34 +0900

    test : CreateUserController test 추가

---
+++ slipp/src/test/java/slipp/controller/CreateUserControllerTest.java
@@ -0,0 +1,45 @@
+package slipp.controller;
+
+import nextstep.mvc.tobe.RequestMappingHandlerMapping;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import slipp.ManualLegacyHandlerMapping;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CreateUserControllerTest {
+    private static final Logger logger = LoggerFactory.getLogger(UserAcceptanceTest.class);
+
+    private RequestMappingHandlerMapping mappings;
+
+    @BeforeEach
+    void setUp() {
+        mappings = new RequestMappingHandlerMapping(new ManualLegacyHandlerMapping());
+        mappings.initialize();
+    }
+
+    @Test
+    @DisplayName("레거시 컨트롤러")
+    void crud() throws Exception {
+        // 회원가입
+        User expected = new User("sloth", "password", "redman", "marx@communism.rus");
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/users/create");
+        request.setParameter("userId", expected.getUserId());
+        request.setParameter("password", expected.getPassword());
+        request.setParameter("name", expected.getName());
+        request.setParameter("email", expected.getEmail());
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        mappings.handle(request, response);
+
+        assertThat(DataBase.findUserById("sloth")).isEqualTo(expected);
+    }
+}
