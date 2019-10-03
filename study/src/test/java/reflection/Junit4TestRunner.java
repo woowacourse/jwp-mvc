16624bdaddff6929293cde0036dddc975a30366d
DaejunBae <dpudpu11@gmail.com>
2019-10-03 21:12:56 +0900

    feat: Java Reflection 실습

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,11 +2,24 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
+        final Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Stream.of(clazz.getMethods())
+                .filter(x -> x.isAnnotationPresent(MyTest.class))
+                .forEach(x -> {
+                    try {
+                        x.invoke(junit4Test);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -5,39 +5,61 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
     public void showClass() {
-        Class<Question> clazz = Question.class;
+        final Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
 
         // TODO Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+        Stream.of(clazz.getDeclaredFields()).forEach(x -> logger.debug("field: {}", x.toString()));
+        Stream.of(clazz.getDeclaredConstructors()).forEach(x -> logger.debug("Constructor: {}", x.toString()));
+        Stream.of(clazz.getDeclaredMethods()).forEach(x -> logger.debug("method: {}", x.toString()));
     }
 
     @Test
     @SuppressWarnings("rawtypes")
     public void constructor_with_args() throws Exception {
-        Class<Question> clazz = Question.class;
-        Constructor[] constructors = clazz.getConstructors();
-        for (Constructor constructor : constructors) {
-            Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
-            for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
-            }
-        }
-
         // TODO 인자를 가진 생성자를 활용해 인스턴스를 생성한다.
+        for (final Constructor<?> constructor : Question.class.getConstructors()) {
+            logger.debug("constructor: {}", constructor);
+            final List<Object> paramTypes = new ArrayList<>();
+            for (final Class<?> parameterType : constructor.getParameterTypes()) {
+                if (parameterType.isPrimitive()) {
+                    paramTypes.add(parameterType.getTypeName().equals("boolean") ? true : 0);
+                } else {
+                    paramTypes.add(parameterType.newInstance());
+                }
+            }
+            final Object object = constructor.newInstance(paramTypes.toArray());
+            logger.debug(object.toString());
+        }
     }
 
     @Test
     public void privateFieldAccess() {
-        Class<Student> clazz = Student.class;
+        final Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
 
         // TODO Student private field에 값을 저장하고 조회한다.
+        try {
+            final Field field = clazz.getDeclaredField("name");
+            final Student student = new Student();
+            final String name = "재성";
+            field.setAccessible(true);
+            field.set(student, name);
+            assertThat(student.getName()).isEqualTo(name);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
