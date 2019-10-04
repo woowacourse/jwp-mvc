924e6d070b38bcf2d8551e6ac87969d32c12b83f
pkch93 <star_final@naver.com>
2019-10-04 20:07:28 +0900

    feat: Junit3Test의 메서드 실행 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,11 +2,19 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] testMethods = clazz.getDeclaredMethods();
+        for (Method testMethod : testMethods) {
+            testMethod.invoke(clazz.newInstance());
+        }
     }
 }
