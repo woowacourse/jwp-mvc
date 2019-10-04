0e10617169bea12b45c3acfd4e198282942f6500
kangmin46 <kangmin789@naver.com>
2019-10-04 15:49:18 +0900

    [feat] : add post user Test

--- slipp/src/test/java/slipp/tobe/controller/UserControllerTest.java
+++ slipp/src/test/java/slipp/tobe/controller/UserControllerTest.java
@@ -3,6 +3,8 @@ package slipp.tobe.controller;
 import org.apache.catalina.startup.Tomcat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import slipp.domain.User;
 import slipp.support.db.DataBase;
@@ -10,11 +12,15 @@ import support.test.NsWebTestClient;
 import support.test.NsWebTestServer;
 
 import java.io.File;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
+    private static final Logger logger = LoggerFactory.getLogger(UserControllerTest.class);
     private NsWebTestClient nsWebTestClient;
 
     @BeforeEach
@@ -26,10 +32,18 @@ class UserControllerTest {
 
     @Test
     void getUserProfile() {
-        User user = new User("1","password", "robby", "robby@naver.com");
-        DataBase.addUser(user);
-        EntityExchangeResult<byte[]> result = nsWebTestClient.getResponse("/users?userId=1");
+        EntityExchangeResult<byte[]> result = nsWebTestClient.getResponse("/users?userId=admin");
         String response = result.toString();
-        assertThat(response).contains("robby");
+        assertThat(response).contains("자바지기");
+    }
+
+    @Test
+    void postUser() {
+        Map<String, String> params = new HashMap<>();
+        params.put("userId", "1");
+        params.put("name", "무민");
+        params.put("password", "password");
+        params.put("email", "moomin@woowahan.com");
+        assertThat(nsWebTestClient.postForm("/users", params)).isEqualTo(URI.create("/"));
     }
 }
