c0df64c1abbe1cd270f9222d60a75528c895ed49
CODEMCD <psbum1609@gmail.com>
2019-10-04 12:48:14 +0900

    test: reflection 을 이용한 클래스 정보 출력 학습 테스트

--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -14,7 +16,20 @@ public class ReflectionTest {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
 
-        // TODO Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            logger.debug("Field: {}", field.getName());
+        }
+
+        Constructor[] constructors = clazz.getConstructors();
+        for (Constructor constructor : constructors) {
+            logger.debug("Constructor: {}", constructor);
+        }
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            logger.debug("Method: {}", method.getName());
+        }
     }
 
     @Test
