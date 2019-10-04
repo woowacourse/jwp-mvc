cdd6dc276ef41c8c2a1528fc2bebbb38e0ad354e
김성훈 <seonghun127@gmail.com>
2019-10-04 19:04:48 +0900

    feat: add controller scanner

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -3,10 +3,9 @@ package nextstep.mvc.tobe;
 import com.google.common.collect.Maps;
 import nextstep.mvc.HandlerMapping;
 import nextstep.mvc.exception.MappingException;
-import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
-import org.reflections.Reflections;
+import org.reflections.ReflectionUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
@@ -23,45 +22,46 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        Reflections reflection = new Reflections(basePackage);
-        Set<Class<?>> controllers = reflection.getTypesAnnotatedWith(Controller.class);
+        ControllerScanner scanner = new ControllerScanner(basePackage);
+        Set<Class<?>> controllers = scanner.getControllers();
 
         for (Class<?> controller : controllers) {
-            Method[] methods = controller.getDeclaredMethods();
+            Object newInstance = scanner.getController(controller);
+            Set<Method> methods = ReflectionUtils.getAllMethods(controller,
+                    ReflectionUtils.withAnnotation(RequestMapping.class));
+
             for (Method method : methods) {
-                putHandlerExecution(controller, method);
+                String url = method.getAnnotation(RequestMapping.class).value();
+                RequestMethod requestMethod = method.getAnnotation(RequestMapping.class).method();
+
+                if (requestMethod.isAll()) {
+                    putAllRequestMethod(newInstance, method, url);
+                    return;
+                }
+
+                putHandler(newInstance, method, new HandlerKey(url, requestMethod));
             }
         }
+    }
+
+    private void putAllRequestMethod(Object instance, Method method, String url) {
+        for (RequestMethod value : RequestMethod.values()) {
+            HandlerKey handlerKey = new HandlerKey(url, value);
+            if (handlerExecutions.containsKey(handlerKey)) {
+                throw new MappingException();
+            }
+            putHandler(instance, method, handlerKey);
+        }
+    }
+
+    private void putHandler(Object instance, Method method, HandlerKey handlerKey) {
+        handlerExecutions.put(handlerKey,
+                (request, response) -> method.invoke(instance, request, response));
     }
 
     @Override
     public nextstep.mvc.asis.Controller getHandler(HttpServletRequest request) {
         HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.of(request.getMethod()));
         return handlerExecutions.get(handlerKey);
-    }
-
-    private void putHandlerExecution(Class<?> controller, Method method) {
-        if (method.isAnnotationPresent(RequestMapping.class)) {
-            String url = method.getAnnotation(RequestMapping.class).value();
-            RequestMethod requestMethod = method.getAnnotation(RequestMapping.class).method();
-            if (requestMethod.isAll()) {
-                putAllMethodMapping(controller, method, url);
-                return;
-            }
-            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
-            handlerExecutions.put(handlerKey,
-                    (request, response) -> method.invoke(controller.newInstance(), request, response));
-        }
-    }
-
-    private void putAllMethodMapping(Class<?> controller, Method method, String url) {
-        for (RequestMethod value : RequestMethod.values()) {
-            HandlerKey handlerKey = new HandlerKey(url, value);
-            if (handlerExecutions.containsKey(handlerKey)) {
-                throw new MappingException();
-            }
-            handlerExecutions.put(handlerKey,
-                    (request, response) -> method.invoke(controller.newInstance(), request, response));
-        }
     }
 }
---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/ControllerScanner.java
@@ -0,0 +1,43 @@
+package nextstep.mvc.tobe;
+
+import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+public class ControllerScanner {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerScanner.class);
+
+    private final Map<Class<?>, Object> controllers;
+
+    public ControllerScanner(Object[] basePackage) {
+        controllers = Maps.newHashMap();
+        scan(basePackage);
+    }
+
+    private void scan(Object[] basePackage) {
+        Reflections reflection = new Reflections(basePackage);
+        Set<Class<?>> annotated = reflection.getTypesAnnotatedWith(Controller.class);
+
+        for (Class<?> controller : annotated) {
+            try {
+                controllers.put(controller, controller.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                logger.debug(e.getMessage());
+            }
+        }
+    }
+
+    public Object getController(Class<?> key) {
+        return controllers.get(key);
+    }
+
+    public Set<Class<?>> getControllers() {
+        return Collections.unmodifiableMap(controllers).keySet();
+    }
+}
