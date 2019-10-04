9cada03479f6832b8b05e6f8de364d21c70d6dd3
hyunsoojung <jhs695@naver.com>
2019-10-04 17:22:12 +0900

    refactor: collectHandlerExecutions() 메소드 분리

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,7 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.mvc.HandlerMapping;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
@@ -10,10 +11,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private Object[] basePackage;
@@ -24,26 +26,36 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
-    public void initialize() throws IllegalAccessException, InstantiationException {
+    @Override
+    public void initialize() {
         Reflections reflections = new Reflections(basePackage);
         Set<Class<?>> clazz = reflections.getTypesAnnotatedWith(Controller.class);
 
-        for (Class<?> claz : clazz) {
-            Object object = claz.newInstance();
-            Method[] methods = claz.getMethods();
-            for (Method method : methods) {
-                if (method.isAnnotationPresent(RequestMapping.class)) {
+        try {
+            for (Class<?> claz : clazz) {
+                Object object = claz.newInstance();
+                Method[] methods = claz.getMethods();
+                collectHandlerExecutions(object, methods);
+            }
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void collectHandlerExecutions(Object object, Method[] methods) {
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> {
                     RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
 
                     HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method());
                     HandlerExecution handlerExecution = new HandlerExecution(object, method);
 
                     handlerExecutions.put(handlerKey, handlerExecution);
-                }
-            }
-        }
+                });
     }
 
+    @Override
     public HandlerExecution getHandler(HttpServletRequest request) {
         return handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
     }
