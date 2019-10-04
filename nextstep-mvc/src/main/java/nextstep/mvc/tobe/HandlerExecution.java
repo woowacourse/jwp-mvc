0f551410714cca8758fda0dc8420bbf646f7b4c6
yk1028 <wongeunsong94@gmail.com>
2019-10-04 15:26:21 +0900

    refactor: HandlerExecution에서 생성자에서 미리 invoke시 사용할 객체 생성

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
@@ -1,21 +1,31 @@
 package nextstep.mvc.tobe;
 
 import nextstep.mvc.asis.Execution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 
 public class HandlerExecution implements Execution {
+    private static final Logger log = LoggerFactory.getLogger(HandlerExecution.class);
+
     private Method method;
+    private Object instance;
 
     public HandlerExecution(Method method) {
         this.method = method;
+        try {
+            instance = method.getDeclaringClass().newInstance();
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
     }
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return (String) method.invoke(method.getDeclaringClass().newInstance(), request, response);
+        return (String) method.invoke(instance, request, response);
     }
 
     @Override
