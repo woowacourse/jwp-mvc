fd7cb95413e66ebfc1db8bfdc6733b3c730b50e2
CODEMCD <psbum1609@gmail.com>
2019-10-04 16:14:19 +0900

    feat: RequestMapping 애노테이션이 추가된 메서드 처리

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,15 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.reflections.Reflections;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
@@ -20,6 +25,25 @@ public class AnnotationHandlerMapping {
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : classes) {
+            Method[] methods = clazz.getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(RequestMapping.class)) {
+                    String pathInfo = request.getRequestURI();
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    if (annotation.value().equals(pathInfo)) {
+                        RequestMethod[] requestMethods = annotation.method();
+                        for (RequestMethod requestMethod : requestMethods) {
+                            if (requestMethod.toString().equals(request.getMethod().toUpperCase())) {
+                                return new HandlerExecution(method);
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return null;
     }
 }
--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
@@ -2,9 +2,16 @@ package nextstep.mvc.tobe;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private final Method method;
+
+    public HandlerExecution(Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(method.getDeclaringClass().getDeclaredConstructor().newInstance(), request, response);
     }
 }
