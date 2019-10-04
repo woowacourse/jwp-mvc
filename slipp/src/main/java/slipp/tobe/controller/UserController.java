95fe261eeded3e3eaa830d5ff37af4e4fa4375ff
kangmin46 <kangmin789@naver.com>
2019-10-04 15:50:04 +0900

    [refactor] : apply JSPView in UserController

--- slipp/src/main/java/slipp/tobe/controller/UserController.java
+++ slipp/src/main/java/slipp/tobe/controller/UserController.java
@@ -1,5 +1,6 @@
 package slipp.tobe.controller;
 
+import nextstep.mvc.tobe.JSPView;
 import nextstep.mvc.tobe.ModelAndView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
@@ -17,23 +18,22 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    ModelAndView get(HttpServletRequest req, HttpServletResponse res) {
+    public ModelAndView get(HttpServletRequest req, HttpServletResponse res) {
         String userId = req.getParameter("userId");
         User user = DataBase.findUserById(userId);
         if (user == null) {
             throw new NullPointerException("사용자를 찾을 수 없습니다.");
         }
         req.setAttribute("user", user);
-        return null;
+        return new ModelAndView(new JSPView("/user/profile.jsp"));
     }
 
     @RequestMapping(value = "/users", method = RequestMethod.POST)
-    ModelAndView post(HttpServletRequest req, HttpServletResponse res) {
+    public ModelAndView post(HttpServletRequest req, HttpServletResponse res) {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
             req.getParameter("email"));
         logger.debug("User : {}", user);
-
         DataBase.addUser(user);
-        return null;
+        return new ModelAndView(new JSPView("redirect:/"));
     }
 }
