0b7a595464a94398d2c63231a23814d5b82acea0
ep1stas1s <masloz@konkuk.ac.kr>
2019-10-06 04:03:52 +0900

    refactor(JspView): support auto suffix

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/JspView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/JspView.java
@@ -4,6 +4,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.util.Objects;
 
 public class JspView implements View {
     private static final String SUFFIX = ".jsp";
@@ -11,6 +12,10 @@ public class JspView implements View {
     private final String view;
 
     public JspView(String view) {
+        if (view.endsWith(SUFFIX)) {
+            this.view = view;
+            return;
+        }
         this.view = view + SUFFIX;
     }
 
@@ -22,5 +27,25 @@ public class JspView implements View {
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         RequestDispatcher rd = request.getRequestDispatcher(view);
         rd.forward(request, response);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JspView jspView = (JspView) o;
+        return Objects.equals(view, jspView.view);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(view);
+    }
+
+    @Override
+    public String toString() {
+        return "JspView{" +
+                "view='" + view + '\'' +
+                '}';
     }
 }
---
+++ nextstep-mvc/src/test/java/nextstep/mvc/tobe/JspViewTest.java
@@ -0,0 +1,12 @@
+package nextstep.mvc.tobe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JspViewTest {
+    @Test
+    void constructorPresenceOrAbsenceSuffix() {
+        assertThat(new JspView("/index")).isEqualTo(new JspView("/index.jsp"));
+    }
+}
