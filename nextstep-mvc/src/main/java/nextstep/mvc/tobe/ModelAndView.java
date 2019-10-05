441e070e658ea6e2fcce37844e029e6626155c77
Jinwoo Shin <bluesky0255@gmail.com>
2019-10-05 12:25:18 +0900

    Implement json view

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/ModelAndView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/ModelAndView.java
@@ -4,16 +4,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ModelAndView {
     private View view;
     private Map<String, Object> model = new HashMap<>();
+
+    public ModelAndView() {
+    }
 
     public ModelAndView(View view) {
         this.view = view;
     }
 
     public void renderView(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        Objects.requireNonNull(view);
         view.render(model, req, resp);
     }
 
