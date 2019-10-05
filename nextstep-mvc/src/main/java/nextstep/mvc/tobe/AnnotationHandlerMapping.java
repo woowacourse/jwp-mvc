ee565b22cd4e4da260686742edf4e6c66179c07f
DaejunBae <dpudpu11@gmail.com>
2019-10-05 20:03:47 +0900

    feat: ControllerScanner.class

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -2,65 +2,47 @@ package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
 import nextstep.mvc.HandlerMapping;
-import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
     private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private Object[] basePackage;
+    private Object[] basePackages;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
-    public AnnotationHandlerMapping(Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(Object... basePackages) {
+        this.basePackages = basePackages;
     }
 
     public void initialize() {
-        final Reflections reflections = new Reflections(basePackage);
+        final ControllerScanner scanner = new ControllerScanner(basePackages);
+        for (final Class<?> clazz : scanner.getClasses()) {
+            final Object instance = scanner.getInstance(clazz);
+            initHandlerExecutions(clazz, instance);
+        }
+    }
 
-
-        // controller가 있는 클래스 찾는다
-        // RequestMapping이 있는 메소드를 찾는다.
-        // map에 등록한다
-        // todo 리팩토링
-        reflections.getTypesAnnotatedWith(Controller.class).forEach(controller -> {
-                    try {
-                        final Object instance = controller.getConstructor().newInstance();
-
-                        Stream.of(controller.getDeclaredMethods())
-                                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                                .forEach(method -> {
-                                    logger.debug("controller: {}, method: {}", controller.getName(), method.getName());
-                                    final RequestMapping rm = method.getAnnotation(RequestMapping.class);
-                                    if (rm.method().length == 0) {
-                                        for (final RequestMethod requestMethod : RequestMethod.values()) {
-                                            final HandlerKey handlerKey = new HandlerKey(rm.value(), requestMethod);
-                                            final HandlerExecution handlerExecution = (req, resp) -> (ModelAndView) method.invoke(instance, req, resp);
-                                            handlerExecutions.put(handlerKey, handlerExecution);
-                                        }
-                                    } else {
-                                        for (final RequestMethod requestMethod : rm.method()) {
-                                            final HandlerKey handlerKey = new HandlerKey(rm.value(), requestMethod);
-                                            final HandlerExecution handlerExecution = (req, resp) -> (ModelAndView) method.invoke(instance, req, resp);
-                                            handlerExecutions.put(handlerKey, handlerExecution);
-                                        }
-                                    }
-                                });
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                        e.printStackTrace();
+    private void initHandlerExecutions(final Class<?> clazz, final Object instance) {
+        Stream.of(clazz.getMethods())
+                .filter(x -> x.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> {
+                    logger.debug("controller: {}, method: {}", clazz.getName(), method.getName());
+                    final RequestMapping rm = method.getAnnotation(RequestMapping.class);
+                    final RequestMethod[] requestMethods = rm.method().length == 0 ? RequestMethod.values() : rm.method();
+                    for (final RequestMethod requestMethod : requestMethods) {
+                        final HandlerKey handlerKey = new HandlerKey(rm.value(), requestMethod);
+                        final HandlerExecution handlerExecution = (req, resp) -> (ModelAndView) method.invoke(instance, req, resp);
+                        handlerExecutions.put(handlerKey, handlerExecution);
                     }
-                }
-        );
+                });
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
