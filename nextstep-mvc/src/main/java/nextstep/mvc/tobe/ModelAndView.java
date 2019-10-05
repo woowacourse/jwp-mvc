118468fa3249bffff924aa72815b7f1cce6430eb
ep1stas1s <masloz@konkuk.ac.kr>
2019-10-06 00:54:02 +0900

    feat(ModelAndView): add render method

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/ModelAndView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/ModelAndView.java
@@ -1,18 +1,24 @@
 package nextstep.mvc.tobe;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ModelAndView {
     private View view;
-    private Map<String, Object> model = new HashMap<String, Object>();
+    private Map<String, Object> model = new HashMap<>();
 
     public ModelAndView() {
     }
 
     public ModelAndView(View view) {
         this.view = view;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        view.render(model, request, response);
     }
 
     public ModelAndView addObject(String attributeName, Object attributeValue) {
