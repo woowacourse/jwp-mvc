8e4dc3f621320f97397991737ceb78c494b429d8
hyojaekim <hj00686@naver.com>
2019-10-05 19:28:45 +0900

    feat: RedirectView 추가
    - UpdateLoginController 상수 제거
    - JspView redirect 로직 제거

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/RedirectView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/RedirectView.java
@@ -1,20 +1,18 @@
 package nextstep.mvc.tobe;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
-public class JspView implements View {
+public class RedirectView implements View {
     private String path;
 
-    public JspView(String path) {
+    public RedirectView(String path) {
         this.path = path;
     }
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        RequestDispatcher rd = request.getRequestDispatcher(path);
-        rd.forward(request, response);
+        response.sendRedirect(path);
     }
 }
--- slipp/src/main/java/slipp/controller/tobe/UpdateLoginController.java
+++ slipp/src/main/java/slipp/controller/tobe/UpdateLoginController.java
@@ -2,6 +2,7 @@ package slipp.controller.tobe;
 
 import nextstep.mvc.tobe.JspView;
 import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.RedirectView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
@@ -17,13 +18,11 @@ import javax.servlet.http.HttpSession;
 public class UpdateLoginController {
     private static final String USER_ID = "userId";
     private static final String PASSWORD = "password";
-    private static final String REDIRECT = "redirect:/";
-    private static final String LOGIN_PAGE = "/user/login.jsp";
     private static final String LOGIN_FAILED = "loginFailed";
 
     @RequestMapping(value = "/users/loginForm", method = RequestMethod.GET)
     public ModelAndView loginPage(HttpServletRequest request, HttpServletResponse response) {
-        return new ModelAndView(new JspView(LOGIN_PAGE));
+        return new ModelAndView(new JspView("/user/login.jsp"));
     }
 
     @RequestMapping(value = "/users/login", method = RequestMethod.POST)
@@ -35,17 +34,17 @@ public class UpdateLoginController {
         if (user != null && user.matchPassword(password)) {
             HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
-            return new ModelAndView(new JspView(REDIRECT));
+            return new ModelAndView(new RedirectView("/"));
         }
 
         request.setAttribute(LOGIN_FAILED, true);
-        return new ModelAndView(new JspView(LOGIN_PAGE));
+        return new ModelAndView(new JspView("/user/login.jsp"));
     }
 
     @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
     public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
         session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
-        return new ModelAndView(new JspView(REDIRECT));
+        return new ModelAndView(new RedirectView("/"));
     }
 }
