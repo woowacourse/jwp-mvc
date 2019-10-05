00618a0aceba80a45f4ee396d38dcca6f3ffc910
hyojaekim <hj00686@naver.com>
2019-10-05 17:31:21 +0900

    refactor: Login 메소드 중복된 로직 제거 및 상수 추가

--- slipp/src/main/java/slipp/controller/tobe/UpdateLoginController.java
+++ slipp/src/main/java/slipp/controller/tobe/UpdateLoginController.java
@@ -15,35 +15,37 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class UpdateLoginController {
+    private static final String USER_ID = "userId";
+    private static final String PASSWORD = "password";
+    private static final String REDIRECT = "redirect:/";
+    private static final String LOGIN_PAGE = "/user/login.jsp";
+    private static final String LOGIN_FAILED = "loginFailed";
+
     @RequestMapping(value = "/users/loginForm", method = RequestMethod.GET)
     public ModelAndView loginPage(HttpServletRequest request, HttpServletResponse response) {
-        ModelAndView mov = new ModelAndView(new JspView("/user/login.jsp"));
-        return mov;
+        return new ModelAndView(new JspView(LOGIN_PAGE));
     }
 
     @RequestMapping(value = "/users/login", method = RequestMethod.POST)
     public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
-        String userId = request.getParameter("userId");
-        String password = request.getParameter("password");
+        String userId = request.getParameter(USER_ID);
+        String password = request.getParameter(PASSWORD);
         User user = DataBase.findUserById(userId);
-        if (user == null) {
-            request.setAttribute("loginFailed", true);
-            return new ModelAndView(new JspView("/user/login.jsp"));
-        }
-        if (user.matchPassword(password)) {
+
+        if (user != null && user.matchPassword(password)) {
             HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
-            return new ModelAndView(new JspView("redirect:/"));
+            return new ModelAndView(new JspView(REDIRECT));
         }
 
-        request.setAttribute("loginFailed", true);
-        return new ModelAndView(new JspView("/user/login.jsp"));
+        request.setAttribute(LOGIN_FAILED, true);
+        return new ModelAndView(new JspView(LOGIN_PAGE));
     }
 
     @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
     public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
         session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
-        return new ModelAndView(new JspView("redirect:/"));
+        return new ModelAndView(new JspView(REDIRECT));
     }
 }
