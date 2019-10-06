76610474e55d0a4df371184ed6d3de15ebcd3700
MK S <conatuseus@gmail.com>
2019-10-06 23:18:30 +0900

    [feat] add class associated View

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JspView.java
@@ -0,0 +1,27 @@
+package nextstep.mvc.tobe.view;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+public class JspView implements View {
+
+    public static final String REDIRECT_PREFIX = "redirect:";
+    private String viewName;
+
+    public JspView(final String viewName) {
+        this.viewName = viewName;
+    }
+
+    @Override
+    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            int redirectViewNameStartIndex = REDIRECT_PREFIX.length();
+            response.sendRedirect(viewName.substring(redirectViewNameStartIndex));
+            return;
+        }
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
+    }
+}
