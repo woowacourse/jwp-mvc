7aa445b04f251226ea2d7c578d83a1a3b9253c41
seokhyeonchoi <lkamna12@gmail.com>
2019-10-06 20:40:19 +0900

    feat: JsonView 구현

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -1,12 +1,43 @@
 package nextstep.mvc.tobe.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.web.support.MediaType;
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
 
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write(convertData(model));
+        printWriter.flush();
+        printWriter.close();
+    }
+
+    private String convertData(Map<String, ?> model) throws JsonProcessingException {
+        if(model.isEmpty()) {
+            return StringUtils.EMPTY;
+        }
+
+        if (model.size() == 1) {
+            Object value = model.values()
+                    .iterator()
+                    .next();
+            return OBJECT_MAPPER.writeValueAsString(value);
+        }
+
+        return OBJECT_MAPPER.writeValueAsString(model);
     }
 }
