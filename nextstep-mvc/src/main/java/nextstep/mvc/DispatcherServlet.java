e8c7cf2366bd1d78e5fb558edc8677e695b70ffe
green4469 <green4469@gmail.com>
2019-10-06 17:13:49 +0900

    feat: refactor dispatcher servlet to utilize adaptor's flexibility

--- nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,27 +1,22 @@
 package nextstep.mvc;
 
-import nextstep.mvc.asis.Controller;
 import nextstep.mvc.tobe.HandlerExecution;
-import nextstep.mvc.tobe.exception.HandlerNotExistException;
 import nextstep.mvc.tobe.ModelAndView;
-import nextstep.mvc.tobe.exception.UnsupportedHandlerTypeException;
+import nextstep.mvc.tobe.exception.HandlerNotExistException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
     private final List<HandlerMapping> handlerMappings;
 
     public DispatcherServlet(List<HandlerMapping> handlerMappings) {
@@ -29,19 +24,20 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         handlerMappings.forEach(HandlerMapping::initialize);
     }
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         String requestUri = req.getRequestURI();
         logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
 
         HandlerExecution handler = getHandler(req);
 
         try {
-            handle(req, resp, handler);
+            ModelAndView mav = handler.handle(req, resp);
+            mav.render(req, resp);
         } catch (Exception e) {
             logger.error("Exception : {}", e);
             throw new ServletException(e.getMessage());
@@ -54,40 +50,5 @@ public class DispatcherServlet extends HttpServlet {
             .findFirst()
             .orElseThrow(HandlerNotExistException::new)
             .getHandler(req);
-    }
-
-    private void handle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-        if (handler instanceof Controller) {
-            handleLegacy(req, resp, (Controller) handler);
-            return;
-        }
-
-        if (handler instanceof HandlerExecution) {
-            handleAnnotation(req, resp, (HandlerExecution) handler);
-            return;
-        }
-
-        throw new UnsupportedHandlerTypeException();
-    }
-
-    private void handleAnnotation(HttpServletRequest req, HttpServletResponse resp, HandlerExecution handler) throws Exception {
-        ModelAndView view = handler.handle(req, resp);
-        view.render(req, resp);
-    }
-
-    private void handleLegacy(HttpServletRequest req, HttpServletResponse resp, Controller handler) throws Exception {
-        String viewName = handler.execute(req, resp);
-        move(viewName, req, resp);
-    }
-
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
     }
 }
