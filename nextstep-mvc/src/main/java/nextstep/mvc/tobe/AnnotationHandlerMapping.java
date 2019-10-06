ea7dcdd95da217fcea64bb62c41b24b67b21d22e
MK S <conatuseus@gmail.com>
2019-10-06 18:07:19 +0900

    [refactor] delete unused code

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,8 +1,6 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
-import nextstep.mvc.scanner.ControllerScanner;
-import nextstep.mvc.scanner.RequestMappingScanner;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
 import org.slf4j.Logger;
@@ -12,9 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -26,42 +22,8 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
-    public void initialize() throws InvocationTargetException, IllegalAccessException {
-//        Reflections reflections = new Reflections("nextstep.mvc.tobe");
+    public void initialize()  {
 
-        Map<Class<?>, Object> controllers = new ControllerScanner("nextStep.mvc.tobe").getControllers();
-
-        for (Map.Entry<Class<?>, Object> clazz : controllers.entrySet()) {
-            Set<Method> methods = RequestMappingScanner.getMethods(clazz.getKey());
-
-            methods.stream().forEach(method -> {
-                RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-                Arrays.stream(requestMapping.method()).forEach(requestMethod -> {
-                    handlerExecutions.put(new HandlerKey(requestMapping.value(), requestMethod), );
-                });
-            });
-        }
-
-
-//        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
-
-        log.debug("controller classes: {}", controllerClasses.toString());
-
-//        for (Class<?> controllerClass : controllerClasses) {
-//            for (Method method : controllerClass.getDeclaredMethods()) {
-//                if (method.isAnnotationPresent(RequestMapping.class)) {
-//                    log.debug("method: {}", method);
-//
-//                    HandlerKey handlerKey = makeHandlerKey(method);
-//
-//                    handlerExecutions.put(handlerKey, new HandlerExecution() {
-//                        public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//                            return (ModelAndView) method.invoke(controllerClass.getDeclaredConstructor().newInstance(), request, response);
-//                        }
-//                    });
-//                }
-//            }
-//        }
 
     }
 
