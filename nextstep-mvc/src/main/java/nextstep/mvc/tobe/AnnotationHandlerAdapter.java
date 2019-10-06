bf561e633b5ab395030f9aabc71802cc4f689736
hyogeon <men1219@hanmail.net>
2019-10-07 03:43:28 +0900

    feat: HandlerAdapter 구현 - AnnotationHandlerAdapter 구현 - ManualHandlerAdapter 구현

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerAdapter.java
@@ -0,0 +1,18 @@
+package nextstep.mvc.tobe;
+
+import nextstep.mvc.HandlerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AnnotationHandlerAdapter implements HandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        return ((HandlerExecution)handler).handle(request, response);
+    }
+}
