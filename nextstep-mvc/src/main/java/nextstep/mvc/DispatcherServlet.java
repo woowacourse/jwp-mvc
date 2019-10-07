d4aafd61392237c4f0c3772bcbb09c67f22c69ca
SunHo Van <vsh123@naver.com>
2019-10-07 21:58:18 +0900

    feat: DispatcherServlet ViewResolver 적용

--- nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,7 +1,11 @@
 package nextstep.mvc;
 
-import nextstep.mvc.asis.Controller;
-import nextstep.mvc.tobe.HandlerExecution;
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.handleradapter.HandlerAdapter;
+import nextstep.mvc.tobe.view.View;
+import nextstep.mvc.tobe.viewresolver.ViewResolver;
+import nextstep.utils.ClassUtils;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
@@ -22,6 +27,8 @@ public class DispatcherServlet extends HttpServlet {
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     private List<HandlerMapping> handlerMappings;
+    private List<HandlerAdapter> handlerAdapters;
+    private List<ViewResolver> viewResolvers;
 
     public DispatcherServlet(List<HandlerMapping> handlerMappings) {
         this.handlerMappings = handlerMappings;
@@ -30,6 +37,26 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     public void init() {
         handlerMappings.forEach(HandlerMapping::initialize);
+        handlerAdapters = detectHandlerAdapters();
+        viewResolvers = detectViewResolvers();
+    }
+
+    private List<HandlerAdapter> detectHandlerAdapters() {
+        return new Reflections(this.getClass().getPackage().getName())
+                .getSubTypesOf(HandlerAdapter.class)
+                .stream()
+                .map(ClassUtils::createInstance)
+                .map(handlerAdapter -> (HandlerAdapter) handlerAdapter)
+                .collect(Collectors.toList());
+    }
+
+    private List<ViewResolver> detectViewResolvers() {
+        return new Reflections(this.getClass().getPackage().getName())
+                .getSubTypesOf(ViewResolver.class)
+                .stream()
+                .map(ClassUtils::createInstance)
+                .map(viewResolver -> (ViewResolver) viewResolver)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -38,16 +65,21 @@ public class DispatcherServlet extends HttpServlet {
 
         try {
             Object handler = findHandler(req);
-            if (handler instanceof Controller) {
-                String viewName = ((Controller) handler).execute(req, resp);
-                move(viewName, req, resp);
-                return;
-            }
-            Object modelAndView = ((HandlerExecution) handler).execute(req, resp);
+            HandlerAdapter handlerAdapter = findHandlerAdapter(handler);
+            ModelAndView modelAndView = handlerAdapter.handle(req, resp, handler);
+            View view = resolveView(modelAndView);
+            view.render(modelAndView.getModel(), req, resp);
         } catch (Exception e) {
             logger.error("Exception : {}", e);
             e.printStackTrace();
         }
+    }
+
+    private HandlerAdapter findHandlerAdapter(Object handler) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.isSupport(handler))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     private Object findHandler(HttpServletRequest req) {
@@ -55,6 +87,13 @@ public class DispatcherServlet extends HttpServlet {
                 .map(handlerMapping -> handlerMapping.getHandler(req))
                 .filter(Objects::nonNull)
                 .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private View resolveView(ModelAndView modelAndView) {
+        return viewResolvers.stream()
+                .map(viewResolver -> viewResolver.resolveViewName(modelAndView.getView()))
+                .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
     }
 
