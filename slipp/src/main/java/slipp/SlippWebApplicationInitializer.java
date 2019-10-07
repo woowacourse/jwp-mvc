70eba15ec3d3e9dc10bef24e400ccca98b4902cc
yeji <yjll1019@naver.com>
2019-10-07 20:48:47 +0900

    refactor: DispatcherServlet의 생성자 매개변수 배열에서 List로 변경

--- slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
+++ slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
@@ -1,21 +1,23 @@
 package slipp;
 
+import java.util.Arrays;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 import nextstep.mvc.DispatcherServlet;
 import nextstep.mvc.tobe.AnnotationHandlerMapping;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 public class SlippWebApplicationInitializer implements WebApplicationInitializer {
     private static final Logger log = LoggerFactory.getLogger(SlippWebApplicationInitializer.class);
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(new ManualHandlerMapping(), new AnnotationHandlerMapping());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(
+                Arrays.asList(new ManualHandlerMapping(), new AnnotationHandlerMapping()));
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
         dispatcher.setLoadOnStartup(1);
