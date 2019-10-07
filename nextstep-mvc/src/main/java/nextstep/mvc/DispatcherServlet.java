a09e9baa336e5425e15b22690621d78fff4980b9
김성훈 <seonghun127@gmail.com>
2019-10-07 20:51:07 +0900

    refactor: change to initialize HandlerAdapter by injecting through constructor

--- nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,19 +1,16 @@
 package nextstep.mvc;
 
-import com.google.common.collect.Lists;
-import nextstep.mvc.asis.ControllerHandlerAdapter;
-import nextstep.mvc.tobe.HandlerExecutionHandlerAdapter;
+import nextstep.mvc.exception.NotFoundHandlerAdapter;
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -21,13 +18,13 @@ import java.util.Objects;
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     private List<HandlerMapping> handlerMappings;
+    private List<HandlerAdapter> handlerAdapters;
 
-    public DispatcherServlet(HandlerMapping... rm) {
-        handlerMappings = Lists.newArrayList();
-        handlerMappings.addAll(Arrays.asList(rm));
+    public DispatcherServlet(List<HandlerMapping> handlerMappings, List<HandlerAdapter> handlerAdapters) {
+        this.handlerMappings = handlerMappings;
+        this.handlerAdapters = handlerAdapters;
     }
 
     @Override
@@ -42,8 +39,9 @@ public class DispatcherServlet extends HttpServlet {
         Object handler = findHandler(req);
         HandlerAdapter handlerAdapter = getHandlerAdapter(handler);
         try {
-            String viewName = (String) handlerAdapter.handle(req, resp, handler);
-            move(viewName, req, resp);
+            ModelAndView modelAndView = handlerAdapter.handle(req, resp, handler);
+            View view = modelAndView.getView();
+            view.render(modelAndView.getModel(), req, resp);
         } catch (Throwable e) {
             logger.error("Exception : {}", e);
             throw new ServletException(e.getMessage());
@@ -59,25 +57,9 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     private HandlerAdapter getHandlerAdapter(Object handler) {
-        if (new ControllerHandlerAdapter().supports(handler)) {
-            return new ControllerHandlerAdapter();
-        }
-
-        if (new HandlerExecutionHandlerAdapter().supports(handler)) {
-            return new HandlerExecutionHandlerAdapter();
-        }
-
-        throw new IllegalArgumentException();
-    }
-
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
+        return handlerAdapters.stream()
+                .filter(adapter -> adapter.supports(handler))
+                .findFirst()
+                .orElseThrow(NotFoundHandlerAdapter::new);
     }
 }
---
+++ nextstep-mvc/src/main/java/nextstep/mvc/exception/NotFoundHandlerAdapter.java
@@ -0,0 +1,7 @@
+package nextstep.mvc.exception;
+
+public class NotFoundHandlerAdapter extends RuntimeException {
+    public NotFoundHandlerAdapter() {
+        super("Not Found Handler Adapter!");
+    }
+}
