c0f62ce2af83aa791c2bf9afa3b56f1f09a9e6df
wbluke <pwb0127@naver.com>
2019-10-07 15:22:33 +0900

    test: Jackson에 대한 학습테스트 작성

---
+++ nextstep-mvc/src/test/java/nextstep/learningtest/JacksonTest.java
@@ -0,0 +1,89 @@
+package nextstep.learningtest;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.mvc.tobe.Car;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JacksonTest {
+
+    private static final String TARGET_PATH = "src/test/java/nextstep/learningtest/target";
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    @DisplayName("Java Object를 JSON으로 변환하기")
+    void writeValue() throws IOException {
+        Car car = new Car("red", "renault");
+
+        String path = TARGET_PATH + "/red_car.json";
+        File resultFile = new File(path);
+        objectMapper.writeValue(resultFile, car);
+
+        printFile(path);
+    }
+
+    @Test
+    @DisplayName("JSON을 Java Object로 변환하기")
+    void readValue() throws IOException {
+        Car car = objectMapper.readValue(new File(TARGET_PATH + "/blue_car.json"), Car.class);
+
+        System.out.println(car);
+    }
+
+    @Test
+    @DisplayName("JSON을 jsonNode로 변환하기")
+    void readTree() throws IOException {
+        JsonNode jsonNode = objectMapper.readTree(new File(TARGET_PATH + "/black_car.json"));
+
+        assertThat(jsonNode.get("color").asText()).isEqualTo("black");
+        assertThat(jsonNode.get("none")).isNull();
+    }
+
+    @Test
+    @DisplayName("JSON을 list로 변환하기")
+    void readList() throws IOException {
+        List<Car> cars = objectMapper.readValue(
+                new File(TARGET_PATH + "/json_car_array.json"),
+                new TypeReference<List<Car>>() {
+                });
+
+        assertThat(cars.size()).isEqualTo(2);
+        System.out.println(cars);
+    }
+
+    @Test
+    @DisplayName("JSON을 map으로 변환하기")
+    void readMap() throws IOException {
+        Map<String, Object> cars = objectMapper.readValue(
+                new File(TARGET_PATH + "/black_car.json"),
+                new TypeReference<Map<String, Object>>() {
+                });
+
+        assertThat(cars.get("color")).isEqualTo("black");
+        assertThat(cars.get("type")).isEqualTo("FIAT");
+        System.out.println(cars);
+    }
+
+    private void printFile(String path) throws IOException {
+        try (InputStream input = new BufferedInputStream(new FileInputStream(path))) {
+            byte[] buffer = new byte[8192];
+            for (int length; (length = input.read(buffer)) != -1; ) {
+                System.out.write(buffer, 0, length);
+            }
+        }
+    }
+}
