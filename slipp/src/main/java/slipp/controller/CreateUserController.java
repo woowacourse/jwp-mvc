634f9c208be33493f65ba3bba6f79138366708ba
jinwook <jin942002@naver.com>
2019-10-07 17:11:55 +0900

    refactor: 기존의 Controller 형태로 원복
    createuserController, listusercontroller 클래스가 controller interface를 구현하도록 함.

--- slipp/src/main/java/slipp/controller/CreateUserController.java
+++ slipp/src/main/java/slipp/controller/CreateUserController.java
@@ -1,5 +1,6 @@
 package slipp.controller;
 
+import nextstep.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import slipp.domain.User;
@@ -8,10 +9,10 @@ import slipp.support.db.DataBase;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-public class CreateUserController {
+public class CreateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
 
+    @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
                 req.getParameter("email"));
--- slipp/src/main/java/slipp/controller/ListUserController.java
+++ slipp/src/main/java/slipp/controller/ListUserController.java
@@ -1,11 +1,13 @@
 package slipp.controller;
 
+import nextstep.mvc.asis.Controller;
 import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ListUserController {
+public class ListUserController implements Controller {
+    @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         if (!UserSessionUtils.isLogined(req.getSession())) {
             return "redirect:/users/loginForm";
