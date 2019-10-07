34dcddb3e4bf4dd182e2614cd4c2e783f62e6bfc
seokhyeonchoi <lkamna12@gmail.com>
2019-10-07 17:55:06 +0900

    refactor: error view 변경

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/ErrorView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/ErrorView.java
@@ -1,37 +1,37 @@
 package nextstep.mvc.tobe.view;
 
 import nextstep.mvc.tobe.RequestContext;
+import nextstep.web.support.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Map;
 
 public class ErrorView implements View {
     private static final Logger logger = LoggerFactory.getLogger(ErrorView.class);
-    private static final String DEFAULT_ERROR_VIEW_NAME = "error.jsp";
+    public static final String HTTP_STATUS_KEY = "httpStatus";
     private final String viewName;
 
     public ErrorView(String viewName) {
         this.viewName = viewName;
     }
 
+    private ErrorView() {
+        this.viewName = "default";
+    }
+
     public static View defaultErrorView() {
-        return new ErrorView(DEFAULT_ERROR_VIEW_NAME);
+        return new ErrorView();
     }
 
     @Override
     public void render(Map<String, ?> model, RequestContext requestContext) {
-        HttpServletRequest request = requestContext.getHttpServletRequest();
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-        model.forEach(request::setAttribute);
+        HttpStatus httpStatus = (HttpStatus) model.get(HTTP_STATUS_KEY);
 
         try {
-            requestDispatcher.forward(request, requestContext.getHttpServletResponse());
-        } catch (ServletException | IOException e) {
+            requestContext.getHttpServletResponse().sendError(httpStatus.getStatus(), httpStatus.getPhrase());
+        } catch (IOException e) {
             logger.error("Internal Server Exception : ", e);
             throw new ErrorViewRenderingFailedException();
         }
