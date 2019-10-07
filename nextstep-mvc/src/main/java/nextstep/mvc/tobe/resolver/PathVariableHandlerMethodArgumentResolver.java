11275c623c01ebda30060bef3fabf4021edeeec7
DaejunBae <dpudpu11@gmail.com>
2019-10-08 00:05:44 +0900

    refactor: PathVariableHandlerMethodArgumentResolver Long 외에도 형변환 지원 추가

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/resolver/PathVariableHandlerMethodArgumentResolver.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/resolver/PathVariableHandlerMethodArgumentResolver.java
@@ -1,12 +1,13 @@
 package nextstep.mvc.tobe.resolver;
 
 import nextstep.utils.PathPatternUtils;
+import nextstep.utils.TypeConverter;
 import nextstep.web.annotation.PathVariable;
 import nextstep.web.annotation.RequestMapping;
 import org.springframework.web.util.pattern.PathPattern;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+import java.util.Objects;
 
 public class PathVariableHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
@@ -18,12 +19,13 @@ public class PathVariableHandlerMethodArgumentResolver implements HandlerMethodA
     public Object resolveArgument(final HttpServletRequest request, final MethodParameter methodParameter) {
         final String uri = request.getRequestURI();
         final String path = methodParameter.getMethod().getAnnotation(RequestMapping.class).value();
-
         final PathPattern pathPattern = PathPatternUtils.parse(path);
-        final Map<String, String> uriVariables = pathPattern
-                .matchAndExtract(PathPatternUtils.toPathContainer(uri))
-                .getUriVariables();
 
-        return Long.parseLong(uriVariables.get(methodParameter.getName()));
+        final String value = Objects.requireNonNull(pathPattern
+                .matchAndExtract(PathPatternUtils.toPathContainer(uri)))
+                .getUriVariables()
+                .get(methodParameter.getName());
+
+        return TypeConverter.to(methodParameter.getType()).apply(value);
     }
 }
