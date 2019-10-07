0a3afebb2560ed3f13b73cf711806086f21264ba
김성훈 <seonghun127@gmail.com>
2019-10-07 20:59:31 +0900

    refactor: inject HandlerAdapter list when constructing DispatcherServlet

--- slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
+++ slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
@@ -1,20 +1,25 @@
 package slipp;
 
 import nextstep.mvc.DispatcherServlet;
+import nextstep.mvc.asis.ControllerHandlerAdapter;
 import nextstep.mvc.tobe.AnnotationHandlerMapping;
+import nextstep.mvc.tobe.HandlerExecutionHandlerAdapter;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+import java.util.Arrays;
 
 public class SlippWebApplicationInitializer implements WebApplicationInitializer {
     private static final Logger log = LoggerFactory.getLogger(SlippWebApplicationInitializer.class);
 
     @Override
     public void onStartup(ServletContext servletContext) {
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(new ManualHandlerMapping(), new AnnotationHandlerMapping());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(
+                Arrays.asList(new ManualHandlerMapping(), new AnnotationHandlerMapping()),
+                Arrays.asList(new ControllerHandlerAdapter(), new HandlerExecutionHandlerAdapter()));
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
         dispatcher.setLoadOnStartup(1);
