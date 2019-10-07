f9fa5c3cb4f69ac92ef212a789036c37d99db242
seokhyeonchoi <lkamna12@gmail.com>
2019-10-07 19:03:34 +0900

    refactor: View Name 접근제어자 private으로 수정

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/viewresolver/JspViewResolver.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/viewresolver/JspViewResolver.java
@@ -4,8 +4,7 @@ import nextstep.mvc.tobe.view.JspView;
 import nextstep.mvc.tobe.view.View;
 
 public class JspViewResolver implements ViewResolver {
-
-    public static final String JSP_VIEW_NAME_SUFFIX = ".jsp";
+    private static final String JSP_VIEW_NAME_SUFFIX = ".jsp";
 
     @Override
     public boolean supports(String viewName) {
