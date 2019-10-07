eb438adcd60b55c2e89b2b6ff75f329d02075fd3
김강민 <kangminals5610@gmail.com>
2019-10-07 22:42:23 +0900

    [refactor] change structure in SlippWebApplicationInitializer

--- slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
+++ slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
@@ -2,6 +2,10 @@ package slipp;
 
 import nextstep.mvc.DispatcherServlet;
 import nextstep.mvc.tobe.AnnotationHandlerMapping;
+import nextstep.mvc.tobe.ControllerHandlerAdapter;
+import nextstep.mvc.tobe.HandlerAdapter;
+import nextstep.mvc.tobe.HandlerExecution;
+import nextstep.mvc.tobe.HandlerExecutionAdaptor;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,9 +19,9 @@ public class SlippWebApplicationInitializer implements WebApplicationInitializer
     private static final Logger log = LoggerFactory.getLogger(SlippWebApplicationInitializer.class);
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(Arrays.asList(new ManualHandlerMapping(),
-            new AnnotationHandlerMapping("slipp")));
+    public void onStartup(ServletContext servletContext) {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(Arrays.asList(new ControllerHandlerAdapter(), new HandlerExecutionAdaptor()),
+            Arrays.asList(new ManualHandlerMapping(), new AnnotationHandlerMapping("slipp")) );
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
