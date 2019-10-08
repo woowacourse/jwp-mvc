cda9adaba760d21dc0b68dd46319bbc71cd7d787
gch01410 <gch01410@naver.com>
2019-10-08 14:57:41 +0900

    refactor: RequestMapping 어노테이션의 메서드를 동적으로 handler로 등록

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -10,12 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
     private Object[] basePackage;
-
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
@@ -23,36 +21,37 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        addController("/users", RequestMethod.POST);
-        addController("/users", RequestMethod.GET);
-        addController("/users/loginForm", RequestMethod.GET);
-        addController("/users/login", RequestMethod.POST);
-        addController("/users/logout", RequestMethod.GET);
-    }
-
-    private void addController(String requestUrl, RequestMethod requestMethod) {
         Set<Class<?>> controllers = ControllerScanner.scan(basePackage, Controller.class);
 
         for (Class<?> controller : controllers) {
-            Method[] methods = controller.getMethods();
-            matchRequest(methods, requestUrl, requestMethod).ifPresent(method -> handlerExecutions.put(
-                    new HandlerKey(requestUrl, requestMethod),
-                    new HandlerExecution(controller, method)));
+            Arrays.stream(controller.getMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> registerHandler(controller, method));
         }
     }
 
-    private Optional<Method> matchRequest(Method[] methods, String requestUrl, RequestMethod requestMethod) {
-        return Arrays.stream(methods)
-                .filter(m -> m.isAnnotationPresent(RequestMapping.class)
-                        && m.getAnnotation(RequestMapping.class).method().equals(requestMethod)
-                        && m.getAnnotation(RequestMapping.class).value().equals(requestUrl))
-                .findAny();
+    private void registerHandler(Class<?> controller, Method method) {
+        RequestMethod[] requestMethods = method.getAnnotation(RequestMapping.class).method();
+        String requestUrl = method.getAnnotation(RequestMapping.class).value();
+
+        if (isEmpty(requestMethods)) {
+            Arrays.stream(RequestMethod.values()).forEach(requestMethod -> handlerExecutions.put(
+                    new HandlerKey(requestUrl, requestMethod),
+                    new HandlerExecution(controller, method)));
+            return;
+        }
+        handlerExecutions.put(new HandlerKey(requestUrl, requestMethods[0]), new HandlerExecution(controller, method));
+    }
+
+
+    private boolean isEmpty(RequestMethod[] requestMethods) {
+        return requestMethods.length == 0;
     }
 
     @Override
     public HandlerExecution getHandler(HttpServletRequest request) {
         String uri = request.getRequestURI();
-        RequestMethod method = RequestMethod.of(request.getMethod());
+        RequestMethod method = RequestMethod.valueOf(request.getMethod());
 
         HandlerKey handlerKey = new HandlerKey(uri, method);
         if (handlerExecutions.containsKey(handlerKey)) {
--- nextstep-mvc/src/main/java/nextstep/web/annotation/RequestMethod.java
+++ nextstep-mvc/src/main/java/nextstep/web/annotation/RequestMethod.java
@@ -1,14 +1,5 @@
 package nextstep.web.annotation;
 
-import java.util.Arrays;
-
 public enum RequestMethod {
     GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
-
-    public static RequestMethod of(String requestMethod) {
-        return Arrays.stream(RequestMethod.values())
-                .filter(method -> method.name().equals(requestMethod.toUpperCase()))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
-    }
 }
