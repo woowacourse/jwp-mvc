b47277a0402477f8f4e33f6df3313e90de1b2978
choihz <choixhz@gmail.com>
2019-10-08 11:04:38 +0900

    feat: JsonView 구현

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -1,5 +1,8 @@
 package nextstep.mvc.tobe.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.web.support.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -7,6 +10,21 @@ import java.util.Map;
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String modelAsString = "";
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
 
+        if (model.size() < 1) {
+            return;
+        }
+        if (model.size() == 1) {
+            Object value = model.values().toArray()[0];
+            modelAsString = objectMapper.writeValueAsString(value);
+        }
+        if (model.size() >= 2) {
+            modelAsString = objectMapper.writeValueAsString(model);
+        }
+
+        response.getWriter().write(modelAsString);
     }
 }
