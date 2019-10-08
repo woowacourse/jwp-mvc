25df4b4567e56f79bb9df58b0fe33b9b5aa0878c
yunheehyeon <ok94life@daum.net>
2019-10-08 17:55:21 +0900

    refactor : status 설정 view 로 이전

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -6,16 +6,38 @@ import nextstep.web.support.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
     private static final int EMPTY_DATA = 0;
     private static final int SINGLE_DATA = 1;
 
+    private int status = 200;
+    private String location;
+
+    public JsonView createOK(String location) {
+        this.status = 201;
+        this.location = location;
+        return this;
+    }
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.setStatus(status);
 
+        setLocation(response);
+        setBody(model, response);
+    }
+
+    private void setLocation(HttpServletResponse response) {
+        if (location != null) {
+            response.setHeader("Location", location);
+        }
+    }
+
+    private void setBody(Map<String, ?> model, HttpServletResponse response) throws IOException {
         if (model.size() != EMPTY_DATA) {
             response.getWriter().write(convertObjectToJson(model));
             response.getWriter().flush();
--- slipp/src/main/java/slipp/controller/UserRestController.java
+++ slipp/src/main/java/slipp/controller/UserRestController.java
@@ -7,8 +7,6 @@ import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
 import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import slipp.dto.UserCreatedDto;
 import slipp.dto.UserUpdatedDto;
 import slipp.support.db.DataBase;
@@ -18,23 +16,20 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserRestController {
-    private static final Logger log = LoggerFactory.getLogger(UserRestController.class);
 
     @RequestMapping(value = "/api/users", method = RequestMethod.POST)
     public ModelAndView create(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         UserCreatedDto userCreatedDto = JsonUtils.toObject(IOUtils.toString(req.getReader()), UserCreatedDto.class);
         DataBase.addUser(userCreatedDto.toUser());
 
-        resp.setStatus(201);
-        resp.setHeader("location", "/api/users?userId=" + userCreatedDto.toUser().getUserId());
-        return new ModelAndView(new JsonView());
+        return new ModelAndView(new JsonView().createOK("/api/users?userId=" + userCreatedDto.toUser().getUserId()));
     }
 
     @RequestMapping(value = "/api/users", method = RequestMethod.GET)
     public ModelAndView find(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         ModelAndView modelAndView = new ModelAndView(new JsonView());
         modelAndView.addObject("user", DataBase.findUserById(req.getParameter("userId")));
-        resp.setStatus(200);
+
         return modelAndView;
     }
 
@@ -42,7 +37,7 @@ public class UserRestController {
     public ModelAndView update(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         UserUpdatedDto userUpdatedDto = JsonUtils.toObject(IOUtils.toString(req.getReader()), UserUpdatedDto.class);
         DataBase.addUser(userUpdatedDto.toUser(req.getParameter("userId")));
-        resp.setStatus(200);
+
         return new ModelAndView(new JsonView());
     }
 }
