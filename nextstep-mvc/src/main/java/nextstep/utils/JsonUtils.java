851a913b606580b0889fcaefa4fda515fa9556cc
school0bhy <house1217@ajou.ac.kr>
2019-10-08 23:36:25 +0900

    feat: JsonView 구현

--- nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
+++ nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
@@ -1,22 +1,32 @@
 package nextstep.utils;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.mvc.tobe.ObjectMapperException;
 
 import java.io.IOException;
 
 public class JsonUtils {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
+                .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
+    }
+
     public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
             return objectMapper.readValue(json, clazz);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
         }
+    }
+
+    public static String toJson(Object value) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(value);
     }
 }
