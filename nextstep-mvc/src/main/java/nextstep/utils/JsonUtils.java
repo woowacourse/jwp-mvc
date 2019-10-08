4b3c240da2da470c8ded07bdafb000c59c1dff5b
이두호 <ddu0422@naver.com>
2019-10-09 00:07:52 +0900

    refactor: Object Mapper 초기화

--- nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
+++ nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
@@ -9,9 +9,19 @@ import java.io.InputStream;
 public class JsonUtils {
     public static ObjectMapper objectMapper = new ObjectMapper();
 
+    static {
+        objectMapper.setVisibility(
+                objectMapper.getSerializationConfig()
+                        .getDefaultVisibilityChecker()
+                        .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                        .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                        .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+        );
+    }
+
     public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
         try {
-            return getObjectMapper().readValue(json, clazz);
+            return objectMapper.readValue(json, clazz);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
         }
@@ -19,19 +29,9 @@ public class JsonUtils {
 
     public static <T> T toObject(InputStream inputStream, Class<T> clazz) throws ObjectMapperException {
         try {
-            return getObjectMapper().readValue(inputStream, clazz);
+            return objectMapper.readValue(inputStream, clazz);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
         }
-    }
-
-    private static ObjectMapper getObjectMapper() {
-        objectMapper.setVisibility(
-                objectMapper.getSerializationConfig()
-                        .getDefaultVisibilityChecker()
-                        .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                        .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
-                        .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
-        return objectMapper;
     }
 }
