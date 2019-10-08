4f4919bba40d03b2fcc82d6b36dd7586bfc51f85
yunheehyeon <ok94life@daum.net>
2019-10-08 15:09:57 +0900

    feat : JsonView 구현

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -1,5 +1,9 @@
 package nextstep.mvc.tobe.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.web.support.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -7,6 +11,30 @@ import java.util.Map;
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
 
+        if (model.size() != 0) {
+            response.getWriter().write(ObjectToJson(model));
+            response.getWriter().flush();
+            response.getWriter().close();
+        }
+    }
+
+    private String ObjectToJson(Map<String, ?> model) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if (model.size() == 1) {
+            return convertOneData(model, objectMapper);
+        }
+
+        return objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(model);
+    }
+
+    private String convertOneData(Map<String, ?> model, ObjectMapper objectMapper) throws JsonProcessingException {
+        Object object = model.values().stream()
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+        return objectMapper.writeValueAsString(object);
     }
 }
