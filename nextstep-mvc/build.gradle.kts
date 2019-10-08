b1a5a958e5c1cd29886c1c6446bda3d9fec524e8
backlo <hjun4512@naver.com>
2019-10-08 15:19:11 +0900

    feat: JsonView 구현

--- nextstep-mvc/build.gradle.kts
+++ nextstep-mvc/build.gradle.kts
@@ -1,6 +1,9 @@
 dependencies {
     implementation("javax.servlet:jstl:1.2")
     implementation("javax.servlet:javax.servlet-api:3.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.9.9")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.9.9")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9")
 
     api("com.fasterxml.jackson.core:jackson-databind:2.9.9.1")
     implementation("com.github.jknack:handlebars:4.1.2")
--- nextstep-mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,12 +1,42 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.web.support.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final int ONE_DATA_TO_MODEL_SIZE = 1;
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        if (model.size() != 0) {
+            String content = getContent(model, objectMapper);
+            response.getWriter().write(content);
+            response.getWriter().flush();
+            response.getWriter().close();
+        }
+    }
 
+    private String getContent(Map<String, ?> model, ObjectMapper objectMapper) throws JsonProcessingException {
+        return model.size() == ONE_DATA_TO_MODEL_SIZE ?
+                objectToString(model, objectMapper) : objectToJson(model, objectMapper);
+    }
+
+    private String objectToString(Map<String, ?> model, ObjectMapper objectMapper) throws JsonProcessingException {
+        Object value = model.values().stream()
+                .findFirst()
+                .orElseThrow(ObjectToStringException::new);
+        return objectMapper.writeValueAsString(value);
+    }
+
+    private String objectToJson(Map<String, ?> model, ObjectMapper objectMapper) throws JsonProcessingException {
+        return objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(model);
     }
 }
