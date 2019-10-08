4b0a1151f44df650a1c434f1870918e7ae395663
mrkwon <erased11@gmail.com>
2019-10-08 14:25:11 +0900

    feat: 2단계 - JsonView 를 구현한다.

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -1,12 +1,25 @@
 package nextstep.mvc.tobe.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.web.support.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter writer = response.getWriter();
+        if (model.isEmpty()) {
+            return;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        String result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
+        writer.write(result);
+        writer.flush();
+        writer.close();
     }
 }
