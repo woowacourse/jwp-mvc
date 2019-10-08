67106944ca359381b3d62a9271ca7c246060eea7
DaejunBae <dpudpu11@gmail.com>
2019-10-09 02:30:48 +0900

    feat: AbstractRequestMappingAdapter
    ResponseBodyAdapter, HandlerExecutionAdapter 중복제거를 위해서 템플릿 메소드 패턴 적용

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/adapter/HandlerExecutionAdapter.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/adapter/HandlerExecutionAdapter.java
@@ -1,33 +1,12 @@
 package nextstep.mvc.tobe.adapter;
 
 import nextstep.mvc.tobe.ModelAndView;
-import nextstep.mvc.tobe.WebRequest;
 import nextstep.mvc.tobe.handler.HandlerExecution;
-import nextstep.mvc.tobe.resolver.HandlerMethodArgumentResolverComposite;
-import nextstep.mvc.tobe.resolver.MethodParameters;
 
-public class HandlerExecutionAdapter implements HandlerAdapter {
-    private HandlerMethodArgumentResolverComposite argumentResolvers;
-
-    public HandlerExecutionAdapter() {
-        argumentResolvers = HandlerMethodArgumentResolverComposite.getInstance();
-    }
+public class HandlerExecutionAdapter extends AbstractRequestMappingAdapter {
 
     @Override
-    public ModelAndView handle(final WebRequest webRequest, final Object handler) throws Exception {
-        final HandlerExecution handlerExecution = (HandlerExecution) handler;
-        final MethodParameters methodParameters = new MethodParameters(handlerExecution.getMethod());
-
-        final Object[] values = methodParameters.getMethodParams().stream()
-                .map(parameter -> argumentResolvers.resolveArgument(webRequest, parameter))
-                .toArray();
-
-        final Object result = handlerExecution.execute(values);
-
-        return parseModelAndView(result);
-    }
-
-    private ModelAndView parseModelAndView(final Object result) {
+    protected ModelAndView resolveReturn(final Object result) {
         if (result instanceof String) {
             return new ModelAndView(String.valueOf(result));
         }
--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/adapter/ResponseBodyAdapter.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/adapter/ResponseBodyAdapter.java
@@ -1,32 +1,14 @@
 package nextstep.mvc.tobe.adapter;
 
 import nextstep.mvc.tobe.ModelAndView;
-import nextstep.mvc.tobe.WebRequest;
 import nextstep.mvc.tobe.handler.HandlerExecution;
-import nextstep.mvc.tobe.resolver.HandlerMethodArgumentResolverComposite;
-import nextstep.mvc.tobe.resolver.MethodParameters;
 import nextstep.mvc.tobe.view.JsonView;
 import nextstep.web.annotation.ResponseBody;
 
-public class ResponseBodyAdapter implements HandlerAdapter {
-    private HandlerMethodArgumentResolverComposite argumentResolvers;
+public class ResponseBodyAdapter extends AbstractRequestMappingAdapter {
 
-    public ResponseBodyAdapter() {
-        argumentResolvers = HandlerMethodArgumentResolverComposite.getInstance();
-    }
-
-    // todo 추상화로 중복 제거 or 합쳐야하나?
     @Override
-    public ModelAndView handle(final WebRequest webRequest, final Object handler) throws Exception {
-        final HandlerExecution handlerExecution = (HandlerExecution) handler;
-        final MethodParameters methodParameters = new MethodParameters(handlerExecution.getMethod());
-
-        final Object[] values = methodParameters.getMethodParams().stream()
-                .map(parameter -> argumentResolvers.resolveArgument(webRequest, parameter))
-                .toArray();
-
-        final Object result = handlerExecution.execute(values);
-
+    protected ModelAndView resolveReturn(final Object result) {
         return new ModelAndView(new JsonView(result));
     }
 
