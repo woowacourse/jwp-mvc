67dc9b34518653479d8d2e43003551c1f5dfd999
backlo <hjun4512@naver.com>
2019-10-09 01:03:40 +0900

    refactor: 기존의 HandlerMapping 삭제 및 adapter 삭제 dispatcher servlet 리펙토링

--- nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,9 +1,5 @@
 package nextstep.mvc;
 
-import nextstep.mvc.asis.Controller;
-import nextstep.mvc.handler.AnnotationHandler;
-import nextstep.mvc.handler.HandlerAdapter;
-import nextstep.mvc.handler.LegacyHandler;
 import nextstep.mvc.tobe.HandlerExecution;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.mvc.view.View;
@@ -14,8 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
@@ -23,20 +18,15 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private final List<HandlerMapping> hms;
-    private final List<HandlerAdapter> adapters;
+    private final HandlerMapping hm;
 
-    public DispatcherServlet(HandlerMapping... hm) {
-        this.hms = Arrays.asList(hm);
-        this.adapters = Arrays.asList(
-                new LegacyHandler(Controller.class),
-                new AnnotationHandler(HandlerExecution.class)
-        );
+    public DispatcherServlet(HandlerMapping hm) {
+        this.hm = hm;
     }
 
     @Override
     public void init() {
-        hms.forEach(HandlerMapping::initialize);
+        hm.initialize();
     }
 
     @Override
@@ -53,19 +43,15 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     private ModelAndView handle(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        Object handler = getHandler(req);
-        return adapters.stream()
-                .filter(adapter -> adapter.supports(handler))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 형식입니다."))
-                .handle(req, resp, handler);
+        Object handler = Optional.ofNullable(getHandler(req))
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 핸들러 입니다."));
+
+        return ((HandlerExecution) handler).handle(req, resp);
     }
 
     private Object getHandler(HttpServletRequest req) {
-        return hms.stream()
-                .filter(hm -> hm.getHandler(req) != null)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("URL이 없습니다."))
-                .getHandler(req);
+        Optional.ofNullable(hm.getHandler(req))
+                .orElseThrow(() -> new IllegalArgumentException("URL이 없습니다."));
+        return hm.getHandler(req);
     }
 }
