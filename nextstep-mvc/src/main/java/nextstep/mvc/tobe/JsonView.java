205b56d5a6d5146eeb121d1fe1e1f777a67aa67f
hyunsoojung <jhs695@naver.com>
2019-10-08 13:48:14 +0900

    test: render_no_element() pass

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/JsonView.java
@@ -1,13 +1,19 @@
 package nextstep.mvc.tobe;
 
+import nextstep.web.support.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        response.addHeader("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
     }
 
     @Override
