2f6a6025be56af5bc5b16197b097ab5b736e8f2e
DaejunBae <dpudpu11@gmail.com>
2019-10-09 02:31:24 +0900

    style: JsonView 불필요한 주석 제거

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -25,7 +25,6 @@ public class JsonView implements View {
         this.object = object;
     }
 
-    // todo 리팩토링
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
@@ -54,8 +53,7 @@ public class JsonView implements View {
         final PrintWriter writer = response.getWriter();
 
         if (Objects.isNotNull(body)) {
-            final String json = OBJECT_MAPPER.writeValueAsString(body);
-            writer.println(json);
+            writer.println(OBJECT_MAPPER.writeValueAsString(body));
         }
         writer.println(convert(model));
         writer.flush();
