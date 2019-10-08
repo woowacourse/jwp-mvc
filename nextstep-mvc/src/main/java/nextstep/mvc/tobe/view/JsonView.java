8e300c281eb3fccab5ae27f69c92b9741953aaa5
heebong <heebg2@naver.com>
2019-10-08 15:14:14 +0900

    feat: add gson
    - pass success JsonViewTest

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -1,14 +1,39 @@
 package nextstep.mvc.tobe.view;
 
+import com.google.gson.Gson;
 import nextstep.web.support.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
+    private final Logger logger = LoggerFactory.getLogger(JsonView.class);
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        String body = convertJson(model);
+        logger.debug("model : {}, body : {}", model, body);
+        writeBody(response.getWriter(), body);
+    }
+
+    private String convertJson(Map<String, ?> model) {
+        if (model.size() == 0) {
+            return "";
+        } else if (model.size() == 1) {
+            String key = model.keySet().iterator().next();
+            return new Gson().toJson(model.get(key));
+        }
+        return new Gson().toJson(model);
+    }
+
+    private void writeBody(PrintWriter writer, String body) {
+        writer.write(body);
+        writer.flush();
+        writer.close();
     }
 }
