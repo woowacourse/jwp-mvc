6fa7ee868b23d91b36caf3db5bb1ce2d65bc8a01
aiden <cnk9109@gmail.com>
2019-10-09 21:09:36 +0900

    feat: update onStartup()

--- slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
+++ slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
@@ -1,7 +1,11 @@
 package slipp;
 
 import nextstep.mvc.DispatcherServlet;
+import nextstep.mvc.HandlerMapping;
+import nextstep.mvc.tobe.AnnotationHandlerAdapter;
 import nextstep.mvc.tobe.AnnotationHandlerMapping;
+import nextstep.mvc.tobe.HandlerAdapter;
+import nextstep.mvc.tobe.ManualHandlerAdapter;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +13,17 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import java.util.Arrays;
+import java.util.List;
 
 public class SlippWebApplicationInitializer implements WebApplicationInitializer {
     private static final Logger log = LoggerFactory.getLogger(SlippWebApplicationInitializer.class);
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(new ManualHandlerMapping(), new AnnotationHandlerMapping("slipp.controller"));
+        List<HandlerMapping> handlerMappings = Arrays.asList(new ManualHandlerMapping(), new AnnotationHandlerMapping("slipp.controller"));
+        List<HandlerAdapter> handlerAdapters = Arrays.asList(new ManualHandlerAdapter(), new AnnotationHandlerAdapter());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(handlerMappings, handlerAdapters);
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
         dispatcher.setLoadOnStartup(1);
