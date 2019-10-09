50f3666b76b79541fbf3690bab485fcefb232050
jinwook <jin942002@naver.com>
2019-10-09 21:19:11 +0900

    feat: map을 json으로 바꾸는 학습 테스트 추가

--- nextstep-mvc/src/test/java/nextstep/utils/JacksonTest.java
+++ nextstep-mvc/src/test/java/nextstep/utils/JacksonTest.java
@@ -4,10 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.mvc.tobe.Car;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JacksonTest {
+    private static final Logger log = LoggerFactory.getLogger(JacksonTest.class);
 
     @Test
     void objectToJson() throws JsonProcessingException {
@@ -18,5 +24,17 @@ public class JacksonTest {
         String expected = "{\"color\":\"yellow\",\"type\":\"renault\"}";
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void mapToJson() throws JsonProcessingException {
+        Map<String, Object> map = new HashMap<>();
+        Car car = new Car("yellow", "renault");
+        Car car2 = new Car("red", "porsche");
+        map.put("car", car);
+        map.put("car2", car2);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        log.debug(objectMapper.writeValueAsString(map));
     }
 }
