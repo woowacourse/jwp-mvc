437b104226efd27fbea4e8e6f599dd3af989c121
MK S <conatuseus@gmail.com>
2019-10-09 16:40:00 +0900

    [refactor] change attribute of RequestMapping, api to none

--- slipp/src/main/java/slipp/controller/HomeController.java
+++ slipp/src/main/java/slipp/controller/HomeController.java
@@ -14,7 +14,7 @@ import static nextstep.web.annotation.RequestMethod.GET;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/api/", method = GET)
+    @RequestMapping(value = "/", method = GET)
     public ModelAndView indexPage(HttpServletRequest req, HttpServletResponse resp) {
         ModelAndView modelAndView = new ModelAndView(new JspView("home.jsp"));
         modelAndView.addObject("users", DataBase.findAll());
