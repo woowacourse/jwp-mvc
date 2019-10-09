e6c0632849aa9edf43730ca8b5516486db613900
ep1stas1s <masloz@konkuk.ac.kr>
2019-10-09 20:11:43 +0900

    feat(NotFoundHandlerException): create exception

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/exception/NotFoundHandlerException.java
@@ -0,0 +1,7 @@
+package nextstep.mvc.tobe.exception;
+
+public class NotFoundHandlerException extends RuntimeException {
+    public NotFoundHandlerException(String message) {
+        super(message);
+    }
+}
