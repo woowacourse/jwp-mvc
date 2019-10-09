e6582be01d2e72ec821dcb5abdedca694f4ec8a9
CODEMCD <psbum1609@gmail.com>
2019-10-10 08:39:03 +0900

    feat: UserApiController 구현

---
+++ slipp/src/main/java/slipp/controller/UserApiController.java
@@ -0,0 +1,63 @@
+package slipp.controller;
+
+import com.google.common.io.CharStreams;
+import nextstep.mvc.tobe.JsonView;
+import nextstep.mvc.tobe.JspView;
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.utils.JsonUtils;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.SC_CREATED;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+@Controller
+public class UserApiController {
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.POST)
+    public ModelAndView create(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String jsonBody = CharStreams.toString(request.getReader());
+        User user = JsonUtils.toObject(jsonBody, User.class);
+        DataBase.addUser(user);
+        response.setStatus(SC_CREATED);
+        response.setHeader("Location", "/api/users?userId=pobi");
+        return new ModelAndView(new JspView("/home.jsp"));
+    }
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        String userId = request.getParameter("userId");
+        if (userId == null) {
+            modelAndView.addObject("user", DataBase.findAll());
+        } else {
+            modelAndView.addObject("user", DataBase.findUserById(userId));
+        }
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.PUT)
+    public ModelAndView modify(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String jsonBody = CharStreams.toString(request.getReader());
+        User updateUser = JsonUtils.toObject(jsonBody, User.class);
+
+        String userId = request.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+
+        user.update(updateUser);
+        DataBase.addUser(user);
+
+        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        modelAndView.addObject("user", user);
+        response.setStatus(SC_OK);
+        return modelAndView;
+    }
+}
