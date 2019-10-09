52c24cd9d991c29ad49c70beb750e338ba317951
DaejunBae <dpudpu11@gmail.com>
2019-10-10 00:17:15 +0900

    feat: HandlerInterceptor

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/interceptor/HandlerInterceptor.java
@@ -0,0 +1,16 @@
+package nextstep.mvc.tobe.interceptor;
+
+import nextstep.mvc.tobe.ModelAndView;
+import org.springframework.lang.Nullable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface HandlerInterceptor {
+    default boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    default void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+}
