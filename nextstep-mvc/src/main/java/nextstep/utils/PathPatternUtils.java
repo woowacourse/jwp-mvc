ac3adfd3d7b32411a894f4443de6ee959a132069
DaejunBae <dpudpu11@gmail.com>
2019-10-10 00:40:18 +0900

    feat: InterceptorRegistration

--- nextstep-mvc/src/main/java/nextstep/utils/PathPatternUtils.java
+++ nextstep-mvc/src/main/java/nextstep/utils/PathPatternUtils.java
@@ -1,24 +1,32 @@
 package nextstep.utils;
 
 import org.springframework.http.server.PathContainer;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.util.pattern.PathPattern;
 import org.springframework.web.util.pattern.PathPatternParser;
 
 public class PathPatternUtils {
-    public static final PathPatternParser PATH_PATTERN_PARSER = new PathPatternParser();
+    private static final PathPatternParser PATH_PATTERN_PARSER = new PathPatternParser();
+    private static final PathMatcher PATH_MATCHER = new AntPathMatcher();
 
     private PathPatternUtils() {
     }
 
-    public static PathPattern parse(String path) {
+    public static PathPattern parse(final String path) {
         PATH_PATTERN_PARSER.setMatchOptionalTrailingSeparator(true);
         return PATH_PATTERN_PARSER.parse(path);
     }
 
-    public static PathContainer toPathContainer(String path) {
+    public static PathContainer toPathContainer(final String path) {
         if (path == null) {
             return null;
         }
         return PathContainer.parsePath(path);
+    }
+
+    public static boolean match(String pattern, String path){
+        final boolean match = PATH_MATCHER.match(pattern, path);
+        return match;
     }
 }
