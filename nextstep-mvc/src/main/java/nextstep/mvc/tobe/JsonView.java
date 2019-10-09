7e8f4051c859bb935d15ba52a53a84fb9776d743
starkim06 <starkim06@naver.com>
2019-10-09 16:16:37 +0900

    refactor: JsonView Model 갯수에 따른 분기 처리 추가

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/JsonView.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 import static nextstep.web.support.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -12,14 +12,29 @@ import static nextstep.web.support.MediaType.APPLICATION_JSON_UTF8_VALUE;
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        response.setContentType(APPLICATION_JSON_UTF8_VALUE);
         ObjectMapper objectMapper = new ObjectMapper();
-        model.forEach((k, v) -> {
-            try {
-                objectMapper.writeValue(response.getOutputStream(), model.get(k));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        response.setContentType(APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter writer = response.getWriter();
+
+        if (hasModel(model)) {
+            writer.write(objectMapper.writeValueAsString(getValue(model)));
+        }
+
+        writer.flush();
     }
+
+    private boolean hasModel(Map<String, ?> model) {
+        return model.size() > 0;
+    }
+
+    private Object getValue(Map<String, ?> model) {
+        if (model.size() == 1) {
+            return model.values()
+                    .stream()
+                    .findAny()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
+        return model;
+    }
+
 }
