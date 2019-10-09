ad3f4efdd97ae1feb98dfe5eab306d057e3a9a97
hyojaekim <hj00686@naver.com>
2019-10-09 17:27:42 +0900

    feat: 기존 컨트롤러 로직 -> 어노테이션 기반으로 변경

--- slipp/src/main/java/slipp/controller/CreateUserController.java
+++ slipp/src/main/java/slipp/controller/CreateUserController.java
@@ -1,24 +1,38 @@
 package slipp.controller;
 
-import slipp.domain.User;
-import slipp.support.db.DataBase;
-import nextstep.mvc.asis.Controller;
+import nextstep.mvc.tobe.JspView;
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.RedirectView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CreateUserController implements Controller {
-    private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
+@Controller
+public class CreateUserController {
+    private static final Logger logger = LoggerFactory.getLogger(CreateUserController.class);
 
-    @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
-                req.getParameter("email"));
-        log.debug("User : {}", user);
+    @RequestMapping(value = "/users/form", method = RequestMethod.GET)
+    public ModelAndView signUpPage(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView(new JspView("/user/form.jsp"));
+    }
+
+    @RequestMapping(value = "/users/create", method = RequestMethod.POST)
+    public ModelAndView signUp(HttpServletRequest request, HttpServletResponse response) {
+        User user = new User(request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
+
+        logger.debug("User : {}", user);
 
         DataBase.addUser(user);
-        return "redirect:/";
+        return new ModelAndView(new RedirectView("/"));
     }
 }
--- slipp/src/main/java/slipp/controller/LoginController.java
+++ slipp/src/main/java/slipp/controller/LoginController.java
@@ -1,6 +1,12 @@
 package slipp.controller;
 
-import nextstep.mvc.asis.Controller;
+import nextstep.mvc.tobe.JspView;
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.RedirectView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
+import slipp.controller.UserSessionUtils;
 import slipp.domain.User;
 import slipp.support.db.DataBase;
 
@@ -8,23 +14,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginController implements Controller {
-    @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        String userId = req.getParameter("userId");
-        String password = req.getParameter("password");
+@Controller
+public class LoginController {
+    private static final String USER_ID = "userId";
+    private static final String PASSWORD = "password";
+    private static final String LOGIN_FAILED = "loginFailed";
+
+    @RequestMapping(value = "/users/loginForm", method = RequestMethod.GET)
+    public ModelAndView loginPage(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView(new JspView("/user/login.jsp"));
+    }
+
+    @RequestMapping(value = "/users/login", method = RequestMethod.POST)
+    public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
+        String userId = request.getParameter(USER_ID);
+        String password = request.getParameter(PASSWORD);
         User user = DataBase.findUserById(userId);
-        if (user == null) {
-            req.setAttribute("loginFailed", true);
-            return "/user/login.jsp";
-        }
-        if (user.matchPassword(password)) {
-            HttpSession session = req.getSession();
+
+        if (user != null && user.matchPassword(password)) {
+            HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
-            return "redirect:/";
-        } else {
-            req.setAttribute("loginFailed", true);
-            return "/user/login.jsp";
+            return new ModelAndView(new RedirectView("/"));
         }
+
+        request.setAttribute(LOGIN_FAILED, true);
+        return new ModelAndView(new JspView("/user/login.jsp"));
+    }
+
+    @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
+    public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
+        return new ModelAndView(new RedirectView("/"));
     }
 }
