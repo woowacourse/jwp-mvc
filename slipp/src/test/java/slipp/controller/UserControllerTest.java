956f4b36b7a2cf95f7f75a9159971f7c2126b8d2
jinwook <jin942002@naver.com>
2019-10-10 14:25:01 +0900

    test: 유저 리스트 화면에 대한 인수 테스트 추가

--- slipp/src/test/java/slipp/controller/UserControllerTest.java
+++ slipp/src/test/java/slipp/controller/UserControllerTest.java
@@ -1,15 +1,55 @@
 package slipp.controller;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserControllerTest {
     private WebTestClient webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
-    ;
+    private String cookie;
+
+    @BeforeAll
+    void setUp() {
+        MultiValueMap<String, String> queryString = new LinkedMultiValueMap<>();
+        queryString.add("userId", "id");
+        queryString.add("password", "password");
+        queryString.add("name", "name");
+        queryString.add("email", "email");
+
+        webTestClient.post().uri("/users/create")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(fromFormData(queryString))
+                .exchange()
+                .expectStatus()
+                .isFound();
+
+        cookie = webTestClient.post().uri("/users/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(fromFormData("userId", "id")
+                        .with("password", "password"))
+                .exchange()
+                .returnResult(String.class).getResponseHeaders().getFirst("Set-Cookie");
+    }
 
     @Test
     void 로그인_안하고_유저_목록_확인() {
         webTestClient.get().uri("/users").exchange().expectStatus().is3xxRedirection();
     }
 
+    @Test
+    void 로그인_하고_유저_목록_확인() {
+        webTestClient.get()
+                .uri("/users")
+                .header("cookie", cookie)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
 }
