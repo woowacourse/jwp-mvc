8ba0c74cde6bf384180ea9920ad17486ee4c1e59
deocksoo <dusol92@snu.ac.kr>
2019-10-10 15:34:00 +0900

    refactor: AnnotationHandlerMapping 리펙토링
    ComponentScan 후에 매핑할 requestMethod 정하고, 정해진 requestMethod에 따라
    일관적으로 Key와 execution을 매핑하도록 수정하였습니다.

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,26 +1,21 @@
 package nextstep.mvc.tobe;
 
 import com.google.common.collect.Maps;
-import nextstep.mvc.tobe.exception.InstanceCreationFailedException;
+import nextstep.mvc.HandlerMapping;
 import nextstep.mvc.tobe.exception.NotFoundHandlerException;
-import nextstep.utils.LoggingUtils;
-import nextstep.web.annotation.Controller;
+import nextstep.mvc.tobe.scanner.ComponentScanner;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
-import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class AnnotationHandlerMapping {
-    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
-
+public class AnnotationHandlerMapping implements HandlerMapping {
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -29,8 +24,9 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
+    @Override
     public void initialize() {
-        List<Object> controllers = getControllers();
+        List<Object> controllers = ComponentScanner.scanControllers(basePackage);
 
         for (Object controller : controllers) {
             Arrays.stream(controller.getClass().getDeclaredMethods())
@@ -39,69 +35,41 @@ public class AnnotationHandlerMapping {
         }
     }
 
-    private List<Object> getControllers() {
-        Reflections reflections = new Reflections(basePackage);
-        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
-        List<Object> controllers = new ArrayList<>();
-
-        for (Class controllerClass : controllerClasses) {
-            controllers.add(instanceOf(controllerClass));
-        }
-        return controllers;
-    }
-
-    private Object instanceOf(Class controllerClass) {
-        try {
-            Constructor constructor = controllerClass.getConstructor();
-            return constructor.newInstance();
-        } catch (NoSuchMethodException | IllegalAccessException
-                | InstantiationException | InvocationTargetException e) {
-            LoggingUtils.logStackTrace(logger, e);
-            throw new InstanceCreationFailedException();
-        }
-    }
-
     private void createHandlerMapping(Object controller, Method method) {
-        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        List<HandlerKey> handlerKeys = getHandlerKeysOf(method);
         HandlerExecution execution = new HandlerExecution(controller, method);
-        RequestMethod[] requestMethods = annotation.method();
 
-        if (requestMethods.length == 0) {
-            connectHandlerToAllConnectableRequestMethods(annotation, execution);
-            return;
-        }
-
-        connectHandler(annotation, execution);
-    }
-
-    private void connectHandlerToAllConnectableRequestMethods(RequestMapping annotation,
-                                                              HandlerExecution execution) {
-        String uri = annotation.value();
-        List<RequestMethod> connectable = Arrays.stream(RequestMethod.values())
-                .filter(requestMethod -> isConnectable(new HandlerKey(uri, requestMethod)))
-                .collect(Collectors.toList());
-
-        HandlerKey handlerKey;
-        for (RequestMethod requestMethod : connectable) {
-            handlerKey = new HandlerKey(uri, requestMethod);
+        for (HandlerKey handlerKey : handlerKeys) {
             handlerExecutions.put(handlerKey, execution);
         }
+    }
+
+    private List<HandlerKey> getHandlerKeysOf(Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        String uri = annotation.value();
+        RequestMethod[] requestMethods = annotation.method();
+
+        Stream<RequestMethod> requestMethodsStream = requestMethods.length == 0
+                ? filterConnectableKeys(uri, RequestMethod.values())
+                : Arrays.stream(requestMethods);
+
+        // TODO: 2019-10-10  다른 컨트롤러에서 똑같은 메서드를 등록한 경우 예외 처리
+        return requestMethodsStream
+                .map(requestMethod -> new HandlerKey(uri, requestMethod))
+                .collect(Collectors.toList());
+    }
+
+    private Stream<RequestMethod> filterConnectableKeys(String uri, RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .filter(requestMethod -> isConnectable(new HandlerKey(uri, requestMethod)));
     }
 
     private boolean isConnectable(HandlerKey handlerKey) {
         return !handlerExecutions.containsKey(handlerKey);
     }
 
-    private void connectHandler(RequestMapping annotation, HandlerExecution execution) {
-        String uri = annotation.value();
-
-        for (RequestMethod requestMethod : annotation.method()) {
-            HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
-            handlerExecutions.put(handlerKey, execution);
-        }
-    }
-
-    public HandlerExecution getHandler(HttpServletRequest request) {
+    @Override
+    public Object getHandler(HttpServletRequest request) {
         String uri = request.getRequestURI();
         RequestMethod method = RequestMethod.valueOf(request.getMethod());
         HandlerExecution handlerExecution = handlerExecutions.get(new HandlerKey(uri, method));
---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/scanner/ComponentScanner.java
@@ -0,0 +1,40 @@
+package nextstep.mvc.tobe.scanner;
+
+import nextstep.mvc.tobe.exception.InstanceCreationFailedException;
+import nextstep.utils.LoggingUtils;
+import nextstep.web.annotation.Controller;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ComponentScanner {
+    private static final Logger logger = LoggerFactory.getLogger(ComponentScanner.class);
+
+    public static List<Object> scanControllers(Object[] basePackage) {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        List<Object> controllers = new ArrayList<>();
+
+        for (Class clazz : controllerClasses) {
+            controllers.add(instanceOf(clazz));
+        }
+        return controllers;
+    }
+
+    private static Object instanceOf(Class controllerClass) {
+        try {
+            Constructor constructor = controllerClass.getConstructor();
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | IllegalAccessException
+                | InstantiationException | InvocationTargetException e) {
+            LoggingUtils.logStackTrace(logger, e);
+            throw new InstanceCreationFailedException();
+        }
+    }
+}
