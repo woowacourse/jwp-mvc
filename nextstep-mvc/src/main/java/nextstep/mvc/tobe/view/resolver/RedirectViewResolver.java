d08e376f8db4703381daf30b01ac4daeec24f00f
박효진 <hgenie14@gmail.com>
2019-10-10 17:18:49 +0900

    feat: Redirect인 경우에 대한 View 및 ViewResolver 구현 클래스 추가

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/resolver/RedirectViewResolver.java
@@ -0,0 +1,16 @@
+package nextstep.mvc.tobe.view.resolver;
+
+import nextstep.mvc.tobe.view.RedirectView;
+import nextstep.mvc.tobe.view.View;
+
+public class RedirectViewResolver implements ViewResolver {
+    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
+
+    @Override
+    public View resolveViewName(String viewName) {
+        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
+            return new RedirectView(viewName);
+        }
+        return null;
+    }
+}
---
+++ nextstep-mvc/src/test/java/nextstep/mvc/tobe/view/resolver/RedirectViewResolverTest.java
@@ -0,0 +1,24 @@
+package nextstep.mvc.tobe.view.resolver;
+
+import nextstep.mvc.tobe.view.RedirectView;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RedirectViewResolverTest {
+
+    @Test
+    void viewName이_redirect일때_리턴값_테스트() {
+        RedirectViewResolver redirectViewResolver = new RedirectViewResolver();
+
+        assertThat(redirectViewResolver.resolveViewName("redirect:/a.jsp")).isInstanceOf(RedirectView.class);
+    }
+
+    @Test
+    void viewName이_redirect가_아닐때_리턴값_테스트() {
+        RedirectViewResolver redirectViewResolver = new RedirectViewResolver();
+
+        assertThat(redirectViewResolver.resolveViewName("index.html")).isNull();
+    }
+
+}
