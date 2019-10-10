74ab912e8b0a43cde0787bddb086f7408363697a
jinwook <jin942002@naver.com>
2019-10-10 18:36:16 +0900

    refactor: manualHandlerMapping 삭제

--- slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
+++ slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
@@ -5,7 +5,6 @@ import nextstep.mvc.HandlerMappingRepository;
 import nextstep.mvc.tobe.handler.AnnotationHandlerAdapter;
 import nextstep.mvc.tobe.handler.AnnotationHandlerMapping;
 import nextstep.mvc.tobe.handler.HandlerAdapterRepository;
-import nextstep.mvc.tobe.handler.ManualHandlerAdapter;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,18 +12,18 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class SlippWebApplicationInitializer implements WebApplicationInitializer {
     private static final Logger log = LoggerFactory.getLogger(SlippWebApplicationInitializer.class);
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        HandlerMappingRepository handlerMappingRepository = new HandlerMappingRepository(Arrays.asList(new ManualHandlerMapping(),
+        HandlerMappingRepository handlerMappingRepository = new HandlerMappingRepository(Collections.singletonList(
                 new AnnotationHandlerMapping("slipp")));
 
         HandlerAdapterRepository handlerAdapterRepository = new HandlerAdapterRepository(
-                Arrays.asList(new ManualHandlerAdapter(), new AnnotationHandlerAdapter())
+                Collections.singletonList(new AnnotationHandlerAdapter())
         );
 
         DispatcherServlet dispatcherServlet = new DispatcherServlet(handlerMappingRepository, handlerAdapterRepository);
