94bc8cc37109f8c30ce0e7b92df883e5c92b2209
Ko eunsuk <eunsukko.128@gmail.com>
2019-10-11 03:01:17 +0900

    [refactor] UpdateFormUserController 어노테이션 적용

--- slipp/src/main/java/slipp/ManualHandlerMapping.java
+++ slipp/src/main/java/slipp/ManualHandlerMapping.java
@@ -24,7 +24,6 @@ public class ManualHandlerMapping implements HandlerMapping {
         mappings.put("/users/login", new LoginController());
         mappings.put("/users/logout", new LogoutController());
         mappings.put("/users/create", new CreateUserController());
-        mappings.put("/users/updateForm", new UpdateFormUserController());
         mappings.put("/users/update", new UpdateUserController());
 
         logger.info("Initialized Request Mapping!");
