25dcb2395d2fd4a9ee6cf19a681f81d23638269e
aiden <cnk9109@gmail.com>
2019-10-10 15:24:27 +0900

    feat: remove move()

--- nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,25 +1,22 @@
 package nextstep.mvc;
 
-import nextstep.mvc.asis.Controller;
-import nextstep.mvc.tobe.*;
+import nextstep.mvc.tobe.HandlerAdapter;
+import nextstep.mvc.tobe.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     private List<HandlerMapping> handlerMappings;
     private List<HandlerAdapter> handlerAdapters;
@@ -31,7 +28,7 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        for (HandlerMapping handlerMapping: handlerMappings) {
+        for (HandlerMapping handlerMapping : handlerMappings) {
             handlerMapping.initialize();
         }
     }
@@ -65,15 +62,5 @@ public class DispatcherServlet extends HttpServlet {
                 .filter(handlerAdapter -> handlerAdapter.supports(handler))
                 .findAny()
                 .orElse(null);
-    }
-
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
     }
 }
