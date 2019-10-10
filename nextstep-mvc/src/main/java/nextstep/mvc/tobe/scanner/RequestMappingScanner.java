3793954ae8279f64274dce307fa7749b6457f7d9
soojinroh <rsj3112@gmail.com>
2019-10-10 17:24:11 +0900

    feat : ControllerScanner, RequestMappingScanner 구현

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/scanner/RequestMappingScanner.java
@@ -0,0 +1,40 @@
+package nextstep.mvc.tobe.scanner;
+
+import nextstep.mvc.tobe.handler.HandlerExecution;
+import nextstep.mvc.tobe.handler.HandlerKey;
+import nextstep.mvc.tobe.handler.RequestMappingHandlerExecution;
+import nextstep.web.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class RequestMappingScanner {
+    private static final Logger log = LoggerFactory.getLogger(RequestMappingScanner.class);
+
+    // todo : refacoring 필요
+    public static Map<HandlerKey, HandlerExecution> scan(Set<Class<?>> annotatedClazz) {
+        Map<HandlerKey, HandlerExecution> handlerExecutions = new HashMap<>();
+
+        for (Class<?> clazz : annotatedClazz) {
+            for (Method method : clazz.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(RequestMapping.class)) {
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    HandlerKey key = new HandlerKey(requestMapping.value(), requestMapping.method());
+                    HandlerExecution execution = null;
+                    try {
+                        execution = new RequestMappingHandlerExecution(method, method.getDeclaringClass().getConstructor().newInstance());
+                    } catch (Exception e) {
+                        log.debug(e.getMessage(), e.getCause());
+                        // TODO : Runtime Exception을 만들어 던지자
+                    }
+                    handlerExecutions.put(key, execution);
+                }
+            }
+        }
+        return handlerExecutions;
+    }
+}
