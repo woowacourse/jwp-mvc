8ac058fa56719d7242e6ce95a1c0366105da0141
MK S <conatuseus@gmail.com>
2019-10-10 20:25:43 +0900

    [refactor] delete as-is code

--- slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
+++ slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
@@ -3,7 +3,6 @@ package slipp;
 import nextstep.mvc.DispatcherServlet;
 import nextstep.mvc.tobe.AnnotationHandlerMapping;
 import nextstep.mvc.tobe.adaptor.AnnotationHandlerAdaptor;
-import nextstep.mvc.tobe.adaptor.ManualHandlerAdaptor;
 import nextstep.mvc.tobe.viewresolver.JsonViewResolver;
 import nextstep.mvc.tobe.viewresolver.JspViewResolver;
 import nextstep.web.WebApplicationInitializer;
@@ -21,8 +20,8 @@ public class SlippWebApplicationInitializer implements WebApplicationInitializer
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         DispatcherServlet dispatcherServlet = new DispatcherServlet(
-            Arrays.asList(new ManualHandlerMapping(), new AnnotationHandlerMapping("slipp")),
-            Arrays.asList(new ManualHandlerAdaptor(), new AnnotationHandlerAdaptor()),
+            Arrays.asList(new AnnotationHandlerMapping("slipp")),
+            Arrays.asList( new AnnotationHandlerAdaptor()),
             Arrays.asList(new JsonViewResolver(), new JspViewResolver()));
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
