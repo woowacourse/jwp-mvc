7e7b7180b4b95bb4a6b713a0d71052e86d100e6d
CODEMCD <psbum1609@gmail.com>
2019-10-10 11:06:51 +0900

    refactor: 유저 생성 컨트롤러 애노테이션 기반으로 변경

---
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -0,0 +1,32 @@
+package slipp.controller;
+
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.RedirectView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/users/create", method = RequestMethod.POST)
+    public ModelAndView create(HttpServletRequest request, HttpServletResponse response) {
+        User user = new User(
+                request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
+        log.debug("User : {}", user);
+
+        DataBase.addUser(user);
+        return new ModelAndView(new RedirectView("redirect:/"));
+    }
+}
