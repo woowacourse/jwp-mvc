346e233330242f2316e0e50dde0091438b4670a6
MK S <conatuseus@gmail.com>
2019-10-10 23:08:11 +0900

    [feat] add 401 response

--- slipp/src/main/java/slipp/controller/LogoutController.java
+++ slipp/src/main/java/slipp/controller/LogoutController.java
@@ -4,18 +4,26 @@ import nextstep.mvc.tobe.view.ModelAndView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import static nextstep.web.annotation.RequestMethod.*;
+
 @Controller
 public class LogoutController {
 
-    @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
-    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    @RequestMapping(value = "/users/logout", method = GET)
+    public ModelAndView logout(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         ModelAndView mav = new ModelAndView();
         HttpSession session = req.getSession();
+        if (session.getAttribute(UserSessionUtils.USER_SESSION_KEY) == null) {
+            resp.setStatus(401);
+            return mav;
+        }
         session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
 
         mav.setViewName("redirect:/");
