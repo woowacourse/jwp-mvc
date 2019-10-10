2e41e4e6142f8b36f3f56b00f0b9f3d60cd56479
김강민 <kangminals5610@gmail.com>
2019-10-10 17:30:54 +0900

    [feat] add Annotation base Controllers

--- slipp/src/main/java/slipp/tobe/controller/UserController.java
+++ slipp/src/main/java/slipp/tobe/controller/UserController.java
@@ -1,5 +1,6 @@
 package slipp.tobe.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.mvc.tobe.JSPView;
 import nextstep.mvc.tobe.ModelAndView;
 import nextstep.web.annotation.Controller;
@@ -7,12 +8,15 @@ import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import slipp.asis.controller.UserSessionUtils;
 import slipp.domain.User;
 import slipp.support.db.DataBase;
 import slipp.tobe.UserNotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class UserController {
@@ -29,12 +33,38 @@ public class UserController {
         return new ModelAndView(new JSPView("/user/profile.jsp"));
     }
 
+    @RequestMapping(value = "/users/updateForm", method = RequestMethod.POST)
+    public ModelAndView updateForm(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        return new ModelAndView(new JSPView("/user/updateForm.jsp"));
+    }
+
     @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public ModelAndView post(HttpServletRequest req, HttpServletResponse res) {
+    public ModelAndView post(HttpServletRequest req, HttpServletResponse res) throws IOException {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
             req.getParameter("email"));
+
         logger.debug("User : {}", user);
         DataBase.addUser(user);
+
+        return new ModelAndView(new JSPView("redirect:/"));
+    }
+
+    @RequestMapping(value = "/users/update", method = RequestMethod.PUT)
+    public ModelAndView update(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        User user = DataBase.findUserById(req.getParameter("userId"));
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+
+        User updateUser = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+            req.getParameter("email"));
+        user.update(updateUser);
         return new ModelAndView(new JSPView("redirect:/"));
     }
 }
