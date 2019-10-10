2305c6c8ba2b7532cd31c2fdef25162cb1c21dbd
kdg <kmdngyu@gmail.com>
2019-10-11 06:47:33 +0900

    feat: json view

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -1,14 +1,35 @@
 package nextstep.mvc.tobe.view;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.mvc.tobe.View;
+import org.springframework.http.MediaType;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
     @Override
-    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) {
+    public void render(Map<String, ?> model, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        resp.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        if(model.isEmpty()){
+            return;
+        }
 
+        if(model.size() == 1 ){
+            Object value = model.values()
+                    .iterator()
+                    .next();
+            String parsedModel = objectMapper.writeValueAsString(value);
+            resp.getWriter().write(parsedModel);
+            return;
+        }
+        String parsedModel = objectMapper.writeValueAsString(model);
+        resp.getWriter().write(parsedModel);
     }
 }
