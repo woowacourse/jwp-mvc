ce5183ded17e366659fd176a7f78cfb7b3a9161f
gch01410 <gch01410@naver.com>
2019-10-10 14:52:23 +0900

    style: 사용하지 않는 throws Exception 삭제

--- slipp/src/main/java/slipp/controller/UpdateUserController.java
+++ slipp/src/main/java/slipp/controller/UpdateUserController.java
@@ -1,10 +1,10 @@
 package slipp.controller;
 
-import slipp.domain.User;
-import slipp.support.db.DataBase;
 import nextstep.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +13,7 @@ public class UpdateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(UpdateUserController.class);
 
     @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
         User user = DataBase.findUserById(req.getParameter("userId"));
         if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
             throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
