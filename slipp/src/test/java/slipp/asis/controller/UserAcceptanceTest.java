ea2ad3f376ec0b71953681619f3993e7c5cc19b5
김강민 <kangminals5610@gmail.com>
2019-10-10 17:34:45 +0900

    [feat] add RestUserController (for UserAccaptanceTest)

--- slipp/src/test/java/slipp/asis/controller/UserAcceptanceTest.java
+++ slipp/src/test/java/slipp/asis/controller/UserAcceptanceTest.java
@@ -1,32 +1,57 @@
 package slipp.asis.controller;
 
+import org.apache.catalina.startup.Tomcat;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import slipp.WebServerLauncher;
 import slipp.dto.UserCreatedDto;
 import slipp.dto.UserUpdatedDto;
 import slipp.domain.User;
+import slipp.tobe.controller.UserController;
 import support.test.NsWebTestClient;
 
+import java.io.File;
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(UserAcceptanceTest.class);
-
+    private Thread serverThread;
     private NsWebTestClient client;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+        serverThread = new Thread(() -> {
+            try {
+                String webappDirLocation = "webapp/";
+                Tomcat tomcat = new Tomcat();
+                tomcat.setPort(8080);
+                String path = new File(webappDirLocation).getAbsolutePath();
+                logger.debug(">> {}" , path);
+                tomcat.addWebapp("/", path);
+                logger.info("configuring app with basedir: {}", new File(webappDirLocation).getAbsolutePath());
+
+                tomcat.start();
+                tomcat.getServer().await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        serverThread.start();
         client = NsWebTestClient.of(8080);
     }
 
     @Test
     @DisplayName("사용자 회원가입/조회/수정/삭제")
     void crud() {
+        logger.debug("abc");
         // 회원가입
         UserCreatedDto expected =
                 new UserCreatedDto("pobi", "password", "포비", "pobi@nextstep.camp");
