f74a36eb3ef126cc2d79239599f5d120dc3831d6
MK S <conatuseus@gmail.com>
2019-10-10 16:53:24 +0900

    [feat] add code for CRU of User

--- slipp/src/main/java/slipp/controller/UserController.java
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -1,6 +1,7 @@
 package slipp.controller;
 
 import nextstep.mvc.tobe.view.ModelAndView;
+import nextstep.utils.JsonUtils;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import org.slf4j.Logger;
@@ -23,31 +24,36 @@ public class UserController {
 
     @RequestMapping(value = "/api/users", method = GET)
     public ModelAndView read(HttpServletRequest request, HttpServletResponse response) {
-        ModelAndView modelAndView = new ModelAndView();
+        ModelAndView mav = new ModelAndView();
         User user = DataBase.findUserById(request.getParameter("userId"));
-        modelAndView.addObject("user", user);
-        return modelAndView;
+        mav.addObject("user", user);
+        return mav;
     }
 
     @RequestMapping(value = "/api/users", method = POST)
     public ModelAndView create(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        User user = new User(request.getParameter("userId"), request.getParameter("password"), request.getParameter("name"),
-            request.getParameter("email"));
-        log.debug("User : {}", user);
+        User user = JsonUtils.toObject(request.getInputStream(), User.class);
         DataBase.addUser(user);
 
         response.setStatus(201);
         response.addHeader("Location", "/api/users?userId=" + user.getUserId());
 
-        log.debug("UserId : {}", user.getUserId());
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("user", user);
-        return modelAndView;
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("user", user);
+        return mav;
     }
 
     @RequestMapping(value = "/api/users", method = PUT)
-    public ModelAndView update(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+    public ModelAndView update(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ModelAndView mav = new ModelAndView();
+
+        String userId = request.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        User updateUser = JsonUtils.toObject(request.getInputStream(), User.class);
+        user.update(updateUser);
+        DataBase.addUser(user);
+        mav.addObject("user", user);
+        return mav;
     }
 
 }
