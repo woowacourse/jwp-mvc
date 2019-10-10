cf945f00a108cdf2a049f177d83c4875de4307ab
박효진 <hgenie14@gmail.com>
2019-10-10 20:48:30 +0900

    refactor: Controller 클래스 메서드 정리

--- slipp/src/main/java/slipp/controller/LoginController.java
+++ slipp/src/main/java/slipp/controller/LoginController.java
@@ -13,6 +13,11 @@ import javax.servlet.http.HttpSession;
 @nextstep.web.annotation.Controller
 public class LoginController {
 
+    @RequestMapping(value = "/users/loginForm", method = RequestMethod.GET)
+    public ModelAndView showLoginForm(HttpServletRequest req, HttpServletResponse resp) {
+        return new ModelAndView("/user/login.jsp");
+    }
+
     @RequestMapping(value = "/users/login", method = RequestMethod.POST)
     public ModelAndView showLogin(HttpServletRequest req, HttpServletResponse resp) {
         String userId = req.getParameter("userId");
@@ -30,5 +35,12 @@ public class LoginController {
             req.setAttribute("loginFailed", true);
             return new ModelAndView("/user/login.jsp");
         }
+    }
+
+    @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
+    public ModelAndView showLogout(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession();
+        session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
+        return new ModelAndView("redirect:/");
     }
 }
--- slipp/src/main/java/slipp/controller/UserController.java
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -16,6 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @RequestMapping(value = "/users/form", method = RequestMethod.GET)
+    public ModelAndView showUserForm(HttpServletRequest req, HttpServletResponse resp) {
+        return new ModelAndView("/user/form.jsp");
+    }
+
     @RequestMapping(value = "/users/create", method = {RequestMethod.POST})
     public ModelAndView createUser(HttpServletRequest req, HttpServletResponse resp) {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
@@ -40,4 +45,35 @@ public class UserController {
         return new ModelAndView("redirect:/");
     }
 
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public ModelAndView showUsers(HttpServletRequest req, HttpServletResponse resp) {
+        if (!UserSessionUtils.isLogined(req.getSession())) {
+            return new ModelAndView("redirect:/users/loginForm");
+        }
+
+        req.setAttribute("users", DataBase.findAll());
+        return new ModelAndView("/user/list.jsp");
+    }
+
+    @RequestMapping(value = "/users/profile", method = RequestMethod.GET)
+    public ModelAndView showProfile(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        return new ModelAndView("/user/profile.jsp");
+    }
+
+    @RequestMapping(value = "/users/updateForm", method = RequestMethod.GET)
+    public ModelAndView updateProfile(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        return new ModelAndView("/user/updateForm.jsp");
+    }
 }
