933861ff06724d164e67f07984c77b1916b72c54
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-10-11 18:34:47 +0900

    test: argument resolvet test 추가

---
+++ nextstep-mvc/src/test/java/nextstep/mvc/argumentresolver/ServletArgumentResolverTest.java
@@ -0,0 +1,48 @@
+package nextstep.mvc.argumentresolver;
+
+import nextstep.mvc.tobe.HandlerExecution;
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.TestUserController;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ServletArgumentResolverTest {
+    private ServletArgumentResolver resolver = new ServletArgumentResolver();
+
+    @Test
+    void request_response_session() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        HttpSession session = request.getSession();
+
+        Class clazz = TestUserController.class;
+        Method method = getMethod("create_request_response", clazz.getDeclaredMethods());
+
+        List<MethodParameter> methodParameters = new HandlerExecution(method, clazz.getDeclaredConstructor().newInstance()).extractMethodParameters();
+
+        ModelAndView mav = (ModelAndView) method.invoke(
+                clazz.getDeclaredConstructor().newInstance(),
+                resolver.resolve(request, response, methodParameters.get(0)),
+                resolver.resolve(request, response, methodParameters.get(1)),
+                resolver.resolve(request, response, methodParameters.get(2)));
+
+        assertThat(mav.getObject("request")).isEqualTo(request);
+        assertThat(mav.getObject("response")).isEqualTo(response);
+        assertThat(mav.getObject("session")).isEqualTo(session);
+    }
+
+    private Method getMethod(String name, Method[] methods) {
+        return Arrays.stream(methods)
+                .filter(method -> method.getName().equals(name))
+                .findFirst()
+                .get();
+    }
+}
