81792efc6ca444204d8cddab26b8b9a2f689789a
강연욱 <fiasboc23@gmail.com>
2019-10-11 16:42:04 +0900

    feat: return early pattern 적용

--- slipp/src/main/java/slipp/controller/LoginController.java
+++ slipp/src/main/java/slipp/controller/LoginController.java
@@ -19,18 +19,14 @@ public class LoginController {
         String userId = req.getParameter("userId");
         String password = req.getParameter("password");
         User user = DataBase.findUserById(userId);
-        if (user == null) {
-            req.setAttribute("loginFailed", true);
-            return new ModelAndView("/user/login.jsp");
-        }
         if (user.matchPassword(password)) {
             HttpSession session = req.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
             return new ModelAndView("redirect:/");
-        } else {
-            req.setAttribute("loginFailed", true);
-            return new ModelAndView("/user/login.jsp");
         }
+
+        req.setAttribute("loginFailed", true);
+        return new ModelAndView("/user/login.jsp");
     }
 
     @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
