5239cb6240725619f16d77c659849325289d6ce9
yunheehyeon <ok94life@daum.net>
2019-10-11 23:07:04 +0900

    refactor : ControllerParameterMapper 클래스 분리

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/ControllerParameterMapper.java
@@ -0,0 +1,75 @@
+package nextstep.mvc.tobe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerParameterMapper {
+    private static final Logger log = LoggerFactory.getLogger(ControllerParameterMapper.class);
+
+    static Map<Class<?>, Function> primitiveCast = new HashMap<>();
+    static {
+        primitiveCast.put(int.class, Integer::valueOf);
+        primitiveCast.put(String.class, (obj)-> obj);
+        primitiveCast.put(long.class, Long::valueOf);
+    }
+
+    private final Method method;
+
+    public ControllerParameterMapper(Method method) {
+        this.method = method;
+    }
+
+    public Object[] getObjects(HttpServletRequest request, HttpServletResponse response) {
+        ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+
+        String[] parameterNames = nameDiscoverer.getParameterNames(method);
+        Class[] classes = method.getParameterTypes();
+        log.debug("Method : {} parameterNames : {}, classes : {}", method, parameterNames, classes);
+
+        Object[] objects = new Object[classes.length];
+
+        for (int i = 0; i < classes.length; i++) {
+            if (primitiveCast.containsKey(classes[i])) {
+                objects[i] = primitiveCast.get(classes[i]).apply(request.getParameter(parameterNames[i]));
+                continue;
+            }
+            if (classes[i].equals(HttpServletRequest.class)) {
+                objects[i] = request;
+                continue;
+            }
+            if (classes[i].equals(HttpServletResponse.class)) {
+                objects[i] = response;
+                continue;
+            }
+            objects[i] = getJavaBean(request, classes[i]);
+        }
+        return objects;
+    }
+
+    private Object getJavaBean(HttpServletRequest request, Class aClass) {
+        try {
+            Object object = aClass.newInstance();
+            for (Field declaredField : aClass.getDeclaredFields()) {
+                declaredField.setAccessible(true);
+                declaredField.set(object, request.getParameter(declaredField.getName()));
+            }
+            return object;
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @FunctionalInterface
+    interface Function<T> {
+        T apply(String t);
+    }
+}
