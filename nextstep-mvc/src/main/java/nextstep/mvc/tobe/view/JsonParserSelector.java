26dcb3eba88a953e6148f88054b07c928f8d46cb
pkch93 <star_final@naver.com>
2019-10-11 16:54:33 +0900

    refactor: ModelToJsonParser 적용 - InvalidModelSizeException 생성자 추가

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonParserSelector.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonParserSelector.java
@@ -1,43 +1,29 @@
 package nextstep.mvc.tobe.view;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.mvc.tobe.view.exception.InvalidModelSizeException;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.function.Function;
+import java.util.*;
 
 public class JsonParserSelector {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final Map<ModelSize, Function<Map<String, ?>, String>> parsers = new HashMap<>();
+    private static final Map<ModelSize, ModelToJsonParser<Map<String, ?>, String>> parsers = new HashMap<>();
+
+    private static final String BLANK = "";
+    private static final int FIRST_VALUE_INDEX = 0;
 
     static {
-        parsers.put(ModelSize.BLANK, model -> "");
+        parsers.put(ModelSize.BLANK, model -> BLANK);
         parsers.put(ModelSize.ONE_SIZE, model -> {
-            TreeMap<String, ?> map = new TreeMap<>(model);
-            String result = "";
-            try {
-                result = objectMapper.writeValueAsString(map.get(map.firstKey()));
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            return result;
+            List<?> temp = new ArrayList<>(model.values());
+            return objectMapper.writeValueAsString(temp.get(FIRST_VALUE_INDEX));
         });
-        parsers.put(ModelSize.MANY_SIZE, model -> {
-            String result = "";
-            try {
-                result = objectMapper.writeValueAsString(model);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            return result;
-        });
+        parsers.put(ModelSize.MANY_SIZE, objectMapper::writeValueAsString);
     }
 
-    static Function<Map<String, ?>, String> getJsonParser(ModelSize modelSize) {
+    static ModelToJsonParser<Map<String, ?>, String> getJsonParser(ModelSize modelSize) {
         if (modelSize == null) {
-            throw new IllegalArgumentException("modelSize가 null이 되면 안됩니다.");
+            throw new InvalidModelSizeException("모델사이즈가 null입니다. 적절한 사이즈를 넣어주세요.");
         }
 
         return parsers.get(modelSize);
--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/exception/InvalidModelSizeException.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/exception/InvalidModelSizeException.java
@@ -4,6 +4,10 @@ public class InvalidModelSizeException extends RuntimeException {
     private static final String ERROR_MESSAGE = "적절하지 않는 Model 사이즈입니다.";
 
     public InvalidModelSizeException() {
-        super(ERROR_MESSAGE);
+        this(ERROR_MESSAGE);
+    }
+
+    public InvalidModelSizeException(String message) {
+        super(message);
     }
 }
