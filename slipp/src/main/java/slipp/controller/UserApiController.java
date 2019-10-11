6551b45823aaa7608bc23d96ab4100123da25f9e
andole87 <andole87@gmail.com>
2019-10-11 16:23:26 +0900

    feat: UserApiController.update() 메서드 생성

--- slipp/src/main/java/slipp/controller/UserApiController.java
+++ slipp/src/main/java/slipp/controller/UserApiController.java
@@ -1,5 +1,6 @@
 package slipp.controller;
 
+import nextstep.mvc.ObjectMapperException;
 import nextstep.mvc.view.JsonView;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.mvc.view.TextView;
@@ -7,8 +8,11 @@ import nextstep.utils.JsonUtils;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import slipp.domain.User;
 import slipp.dto.UserCreatedDto;
+import slipp.dto.UserUpdatedDto;
 import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,11 +22,11 @@ import java.util.stream.Collectors;
 
 @Controller
 public class UserApiController {
+    private static final Logger log = LoggerFactory.getLogger(UserApiController.class);
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/users")
-    public ModelAndView create(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String json = request.getReader().lines()
-                .collect(Collectors.joining());
+    public ModelAndView create(HttpServletRequest request, HttpServletResponse response) {
+        String json = getJsonFromRequestBody(request);
 
         User user = JsonUtils.toObject(json, UserCreatedDto.class).toEntity();
         DataBase.addUser(user);
@@ -40,5 +44,26 @@ public class UserApiController {
         ModelAndView mv = new ModelAndView(new JsonView());
         mv.addObject("user", user);
         return mv;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/api/users")
+    public ModelAndView update(HttpServletRequest request, HttpServletResponse response) {
+        String userId = request.getParameter("userId");
+        String json = getJsonFromRequestBody(request);
+        User updateUser = JsonUtils.toObject(json, UserUpdatedDto.class).toEntity(userId);
+        DataBase.findUserById(userId).update(updateUser);
+
+        return new ModelAndView(new TextView(""));
+    }
+
+
+    private String getJsonFromRequestBody(HttpServletRequest request) {
+        try {
+            return request.getReader().lines()
+                    .collect(Collectors.joining());
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new ObjectMapperException();
+        }
     }
 }
--- slipp/src/test/java/slipp/controller/UserAcceptanceTest.java
+++ slipp/src/test/java/slipp/controller/UserAcceptanceTest.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import slipp.domain.User;
 import slipp.dto.UserCreatedDto;
+import slipp.dto.UserUpdatedDto;
 import support.test.NsWebTestClient;
 
 import java.net.URI;
@@ -37,14 +38,14 @@ public class UserAcceptanceTest {
         assertThat(actual.getUserId()).isEqualTo(expected.getUserId());
         assertThat(actual.getName()).isEqualTo(expected.getName());
         assertThat(actual.getEmail()).isEqualTo(expected.getEmail());
-//
-//        // 수정
-//        UserUpdatedDto updateUser = new UserUpdatedDto("password2", "코난", "conan@nextstep.camp");
-//        client.updateResource(location, updateUser, UserUpdatedDto.class);
-//
-//        actual = client.getResource(location, User.class);
-//        assertThat(actual.getPassword()).isEqualTo(updateUser.getPassword());
-//        assertThat(actual.getName()).isEqualTo(updateUser.getName());
-//        assertThat(actual.getEmail()).isEqualTo(updateUser.getEmail());
+
+        // 수정
+        UserUpdatedDto updateUser = new UserUpdatedDto("password2", "코난", "conan@nextstep.camp");
+        client.updateResource(location, updateUser, UserUpdatedDto.class);
+
+        actual = client.getResource(location, User.class);
+        assertThat(actual.getPassword()).isEqualTo(updateUser.getPassword());
+        assertThat(actual.getName()).isEqualTo(updateUser.getName());
+        assertThat(actual.getEmail()).isEqualTo(updateUser.getEmail());
     }
 }
