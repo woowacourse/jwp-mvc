73e170a28bf59f5e21407e6b328bf72a9240d761
pkch93 <star_final@naver.com>
2019-10-11 16:52:54 +0900

    refactor: JsonParser -> ModelToJsonParser

--- nextstep-mvc/src/test/java/nextstep/mvc/tobe/view/ModelToJsonParserSelectorTest.java
+++ nextstep-mvc/src/test/java/nextstep/mvc/tobe/view/ModelToJsonParserSelectorTest.java
@@ -1,8 +1,11 @@
 package nextstep.mvc.tobe.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Maps;
+import nextstep.mvc.tobe.exception.InvalidHandlerAdaptException;
+import nextstep.mvc.tobe.view.exception.InvalidModelSizeException;
 import nextstep.utils.JsonUtils;
 import org.junit.jupiter.api.Test;
 import samples.Car;
@@ -15,27 +18,27 @@ import java.util.function.Function;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class JsonParserSelectorTest {
+class ModelToJsonParserSelectorTest {
 
     private Map<String, Object> testModel;
 
     @Test
-    void modelSize가_0인_경우_빈_String_반환_테스트() {
+    void modelSize가_0인_경우_빈_String_반환_테스트() throws JsonProcessingException {
         testModel = Collections.emptyMap();
-        Function<Map<String, ?>, String> jsonParser = JsonParserSelector.getJsonParser(ModelSize.BLANK);
+        ModelToJsonParser<Map<String, ?>, String> jsonParser = JsonParserSelector.getJsonParser(ModelSize.BLANK);
 
-        assertThat(jsonParser.apply(testModel)).isEqualTo("");
+        assertThat(jsonParser.parse(testModel)).isEqualTo("");
     }
 
     @Test
-    void modelSize가_1인_경우_value값만_반환_테스트() {
+    void modelSize가_1인_경우_value값만_반환_테스트() throws JsonProcessingException {
         testModel = Maps.newHashMap();
 
         Car expected = new Car("red", "suv");
         testModel.put("car", expected);
 
-        Function<Map<String, ?>, String> jsonParser = JsonParserSelector.getJsonParser(ModelSize.ONE_SIZE);
-        String json = jsonParser.apply(testModel);
+        ModelToJsonParser<Map<String, ?>, String> jsonParser = JsonParserSelector.getJsonParser(ModelSize.ONE_SIZE);
+        String json = jsonParser.parse(testModel);
         Car actual = JsonUtils.toObject(json, Car.class);
 
         assertThat(actual).isEqualTo(expected);
@@ -48,8 +51,8 @@ class JsonParserSelectorTest {
         testModel.put("car", new Car("red", "suv"));
         testModel.put("car2", new Car("blue", "suv"));
 
-        Function<Map<String, ?>, String> jsonParser = JsonParserSelector.getJsonParser(ModelSize.MANY_SIZE);
-        String json = jsonParser.apply(testModel);
+        ModelToJsonParser<Map<String, ?>, String> jsonParser = JsonParserSelector.getJsonParser(ModelSize.MANY_SIZE);
+        String json = jsonParser.parse(testModel);
         ObjectMapper objectMapper = new ObjectMapper();
 
         assertThat(testModel).isEqualTo(objectMapper.readValue(json, new TypeReference<Map<String, Car>>(){}));
@@ -57,6 +60,6 @@ class JsonParserSelectorTest {
 
     @Test
     void modelSize가_null인경우_예외_테스트() {
-        assertThrows(IllegalArgumentException.class, () -> JsonParserSelector.getJsonParser(null));
+        assertThrows(InvalidModelSizeException.class, () -> JsonParserSelector.getJsonParser(null));
     }
 }
