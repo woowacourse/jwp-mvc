5dc6e070d3951bdf03dc009fb22cfdb5872b8561
seokhyeonchoi <lkamna12@gmail.com>
2019-10-11 17:29:36 +0900

    refactor: JsonUtils 수정

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/JsonView.java
@@ -3,31 +3,16 @@ package nextstep.mvc.tobe.view;
 import nextstep.mvc.tobe.RequestContext;
 import nextstep.utils.JsonUtils;
 import nextstep.web.support.MediaType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
-    private static final Logger logger = LoggerFactory.getLogger(JsonView.class);
 
     @Override
     public void render(Map<String, ?> model, RequestContext requestContext) {
         HttpServletResponse response = requestContext.getHttpServletResponse();
-
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-
-        try {
-            PrintWriter printWriter = response.getWriter();
-            printWriter.write(JsonUtils.toJson(model));
-            printWriter.flush();
-            printWriter.close();
-        } catch (IOException e) {
-            logger.error("Http Request Exception : ", e);
-            throw new JsonWritingFailedException();
-        }
+        JsonUtils.writeValue(response, model);
     }
 }
--- nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
+++ nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
@@ -3,12 +3,18 @@ package nextstep.utils;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jknack.handlebars.internal.lang3.StringUtils;
+import nextstep.mvc.tobe.view.JsonWritingFailedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JsonUtils.class);
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
@@ -34,6 +40,10 @@ public class JsonUtils {
         }
     }
 
+    public static String toJson(Object object) throws IOException {
+        return OBJECT_MAPPER.writeValueAsString(object);
+    }
+
     public static String toJson(Map<String, ?> model) throws IOException {
         if (model.isEmpty()) {
             return StringUtils.EMPTY;
@@ -48,5 +58,25 @@ public class JsonUtils {
         }
 
         return OBJECT_MAPPER.writeValueAsString(model);
+    }
+
+    public static void writeValue(HttpServletResponse response, Object value) {
+        try {
+            PrintWriter printWriter = response.getWriter();
+            printWriter.write(parseType(value));
+            printWriter.flush();
+            printWriter.close();
+        } catch (IOException e) {
+            logger.error("Http Request Exception : ", e);
+            throw new JsonWritingFailedException();
+        }
+    }
+
+    private static String parseType(Object value) throws IOException {
+        if (value instanceof Map) {
+            return toJson((Map<String, ?>) value);
+        }
+
+        return toJson(value);
     }
 }
