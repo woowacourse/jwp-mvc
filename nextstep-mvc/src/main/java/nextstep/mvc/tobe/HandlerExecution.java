3bd3f7b536ca5f57701e14fbeb610bb6f724fd96
aiden <cnk9109@gmail.com>
2019-10-11 16:45:08 +0900

    refactor: add logger

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/HandlerExecution.java
@@ -1,10 +1,15 @@
 package nextstep.mvc.tobe;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private static final Logger logger = LoggerFactory.getLogger(HandlerExecution.class);
+
     private Object target;
     private Method method;
 
@@ -15,6 +20,7 @@ public class HandlerExecution {
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String viewName = (String) method.invoke(target, request, response);
+        logger.debug("JSP support() viewName : {}", viewName);
 
         return new ModelAndView(viewName);
     }
