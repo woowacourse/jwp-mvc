d5e9555c06217318ffdf07b3f8f02b13d8bb65a4
aiden <cnk9109@gmail.com>
2019-10-11 16:55:06 +0900

    refactor: create HandlerMappingManger

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/handler/HandlerMappingManager.java
@@ -0,0 +1,22 @@
+package nextstep.mvc.tobe.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class HandlerMappingManager {
+    private static List<HandlerMapping> handlerMappings = new ArrayList<>();
+
+    static {
+        handlerMappings.add(new AnnotationHandlerMapping("slipp.controller"));
+    }
+
+    public static Object getHandlerMapping(HttpServletRequest req) {
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(req))
+                .filter(Objects::nonNull)
+                .findAny()
+                .orElse(null);
+    }
+}
