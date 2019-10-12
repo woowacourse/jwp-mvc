72527e7984cf0fdede5afe6756c5ef88e68605be
gch01410 <gch01410@naver.com>
2019-10-12 18:26:34 +0900

    refactor: 모든 컨트롤러를 애노테이션 기반 MVC로 변경

---
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -0,0 +1,78 @@
+package slipp.controller;
+
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String showUserList(HttpServletRequest request, HttpServletResponse response) {
+        if (!UserSessionUtils.isLogined(request.getSession())) {
+            return "redirect:/users/loginForm";
+        }
+
+        request.setAttribute("users", DataBase.findAll());
+        return "/user/list.jsp";
+    }
+
+    @RequestMapping(value = "/users/form", method = RequestMethod.GET)
+    public String showUserForm(HttpServletRequest request, HttpServletResponse response) {
+        return "/user/form.jsp";
+    }
+
+    @RequestMapping(value = "/users/profile", method = RequestMethod.GET)
+    public String showProfile(HttpServletRequest request, HttpServletResponse response) {
+        String userId = request.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+        }
+        request.setAttribute("user", user);
+        return "/user/profile.jsp";
+    }
+
+    @RequestMapping(value = "/users/updateForm", method = RequestMethod.GET)
+    public String showUpdateForm(HttpServletRequest request, HttpServletResponse response) {
+        String userId = request.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (!UserSessionUtils.isSameUser(request.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+        request.setAttribute("user", user);
+        return "/user/updateForm.jsp";
+    }
+
+    @RequestMapping(value = "/users/create", method = RequestMethod.POST)
+    public String create(HttpServletRequest request, HttpServletResponse response) {
+        User user = new User(request.getParameter("userId"), request.getParameter("password"), request.getParameter("name"),
+                request.getParameter("email"));
+        log.debug("User : {}", user);
+
+        DataBase.addUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/users/update", method = RequestMethod.POST)
+    public String modify(HttpServletRequest req, HttpServletResponse resp) {
+        User user = DataBase.findUserById(req.getParameter("userId"));
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+
+        User updateUser = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+                req.getParameter("email"));
+        log.debug("Update User : {}", updateUser);
+        user.update(updateUser);
+        return "redirect:/";
+    }
+}
