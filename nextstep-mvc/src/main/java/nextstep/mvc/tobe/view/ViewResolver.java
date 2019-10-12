b1806abbda61a9a76a07ff52e67e141e05784df8
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-12 22:28:02 +0900

    test: add ViewTest

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/ViewResolver.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/ViewResolver.java
@@ -1,12 +1,18 @@
 package nextstep.mvc.tobe.view;
 
+import nextstep.mvc.tobe.exception.NoSuchViewException;
+
 public class ViewResolver {
+    private static final String EXTENSION_OF_JSP = ".jsp";
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
 
     public static View resolve(String viewName) {
         if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
             return new RedirectView(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
         }
-        return new JspView(viewName);
+        if (viewName.contains(EXTENSION_OF_JSP)) {
+            return new JspView(viewName);
+        }
+        throw new NoSuchViewException();
     }
 }
