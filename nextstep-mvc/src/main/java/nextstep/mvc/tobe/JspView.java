4fdd1286859d98ae5daa6d1eda8a758955efaaea
hyunsoojung <jhs695@naver.com>
2019-10-14 02:32:59 +0900

    refactor: redirect 부분 제거

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/JspView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/JspView.java
@@ -1,15 +1,11 @@
 package nextstep.mvc.tobe;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Map;
 
 public class JspView implements View {
-    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
-
     private String name;
 
     public JspView(String name) {
@@ -18,21 +14,14 @@ public class JspView implements View {
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        move(name, request, response);
+        model.forEach(request::setAttribute);
+
+        RequestDispatcher rd = request.getRequestDispatcher(name);
+        rd.forward(request, response);
     }
 
     @Override
     public String getName() {
         return name;
-    }
-
-    private void move(String viewName, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
-            resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        RequestDispatcher rd = req.getRequestDispatcher(viewName);
-        rd.forward(req, resp);
     }
 }
