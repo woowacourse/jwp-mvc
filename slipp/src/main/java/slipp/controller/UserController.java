73264baf88791cd94342fa2041bd8909fbae7cbc
hyunsoojung <jhs695@naver.com>
2019-10-13 18:10:17 +0900

    feat: create(), search(), update() 추가

--- slipp/src/main/java/slipp/controller/UserController.java
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -1,27 +1,80 @@
 package slipp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.mvc.tobe.JsonView;
+import nextstep.mvc.tobe.ModelAndView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import slipp.controller.exception.LoginUserException;
+import slipp.controller.exception.NotFoundUserException;
 import slipp.domain.User;
+import slipp.dto.UserCreatedDto;
 import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    @RequestMapping(value = "/users/create", method = RequestMethod.POST)
-    public String create(HttpServletRequest req, HttpServletResponse resp) {
-        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
-                req.getParameter("email"));
-        log.debug("User : {}", user);
+    @RequestMapping(value = "/api/users", method = RequestMethod.POST)
+    public ModelAndView create(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        UserCreatedDto userDto = OBJECT_MAPPER.readValue(req.getReader(), UserCreatedDto.class);
+
+        User user = new User(userDto.getUserId(),
+                userDto.getPassword(),
+                userDto.getName(),
+                userDto.getEmail());
 
         DataBase.addUser(user);
-        return "redirect:/";
+
+        resp.setHeader("Location", "/api/users?userId=" + user.getUserId());
+        resp.setStatus(HttpServletResponse.SC_CREATED);
+
+        return new ModelAndView(new JsonView());
+    }
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    public ModelAndView search(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userId = request.getParameter("userId");
+
+        log.debug("user ID: {}", userId);
+
+        User user = DataBase.findUserById(userId)
+                .orElseThrow(NotFoundUserException::new);
+
+        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        modelAndView.addObject("user", user);
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.PUT)
+    public ModelAndView update(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UserCreatedDto userDto = OBJECT_MAPPER.readValue(request.getReader(), UserCreatedDto.class);
+
+        User updateUser = new User(userDto.getUserId(),
+                userDto.getPassword(),
+                userDto.getName(),
+                userDto.getEmail());
+
+        if (!UserSessionUtils.isLogined(request.getSession())) {
+            throw new LoginUserException();
+        }
+
+        if (UserSessionUtils.isSameUser(request.getSession(), updateUser)) {
+            User user = UserSessionUtils.getUserFromSession(request.getSession());
+            user.update(updateUser);
+        }
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        return new ModelAndView(new JsonView());
     }
 }
