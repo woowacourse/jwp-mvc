89c6931731bf17d5cf3e1481b77e53df02b3326f
hyunsoojung <jhs695@naver.com>
2019-10-14 02:48:38 +0900

    refactor: annotation 기반으로 변경

--- slipp/src/main/java/slipp/controller/ProfileController.java
+++ slipp/src/main/java/slipp/controller/ProfileController.java
@@ -1,6 +1,10 @@
 package slipp.controller;
 
-import nextstep.mvc.asis.Controller;
+import nextstep.mvc.tobe.JspView;
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
 import slipp.controller.exception.NotFoundUserException;
 import slipp.domain.User;
 import slipp.support.db.DataBase;
@@ -8,9 +12,11 @@ import slipp.support.db.DataBase;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ProfileController implements Controller {
-    @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+@Controller
+public class ProfileController {
+
+    @RequestMapping(value = "/users/profile", method = RequestMethod.GET)
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         String userId = req.getParameter("userId");
         User user = DataBase.findUserById(userId)
                 .orElseThrow(NotFoundUserException::new);
@@ -18,7 +24,10 @@ public class ProfileController implements Controller {
         if (user == null) {
             throw new NullPointerException("사용자를 찾을 수 없습니다.");
         }
-        req.setAttribute("user", user);
-        return "/user/profile.jsp";
+
+        ModelAndView modelAndView = new ModelAndView(new JspView("/user/profile.jsp"));
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
     }
 }
