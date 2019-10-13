6b2755ab4493e65f0c695f02a107086e59961bd8
hyunsoojung <jhs695@naver.com>
2019-10-14 02:40:03 +0900

    refactor: rename, view 수정

--- slipp/src/main/java/slipp/controller/UserApiController.java
+++ slipp/src/main/java/slipp/controller/UserApiController.java
@@ -1,8 +1,9 @@
 package slipp.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nextstep.mvc.tobe.JsonView;
+import nextstep.mvc.tobe.JspView;
 import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.RedirectView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
@@ -19,8 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
-public class UserController {
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+public class UserApiController {
+    private static final Logger log = LoggerFactory.getLogger(UserApiController.class);
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @RequestMapping(value = "/api/users", method = RequestMethod.POST)
@@ -37,7 +38,7 @@ public class UserController {
         resp.setHeader("Location", "/api/users?userId=" + user.getUserId());
         resp.setStatus(HttpServletResponse.SC_CREATED);
 
-        return new ModelAndView(new JsonView());
+        return new ModelAndView(new RedirectView("/"));
     }
 
     @RequestMapping(value = "/api/users", method = RequestMethod.GET)
@@ -49,7 +50,7 @@ public class UserController {
         User user = DataBase.findUserById(userId)
                 .orElseThrow(NotFoundUserException::new);
 
-        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        ModelAndView modelAndView = new ModelAndView(new JspView("/user/list.jsp"));
         modelAndView.addObject("user", user);
         response.setStatus(HttpServletResponse.SC_OK);
 
@@ -58,23 +59,21 @@ public class UserController {
 
     @RequestMapping(value = "/api/users", method = RequestMethod.PUT)
     public ModelAndView update(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        UserCreatedDto userDto = OBJECT_MAPPER.readValue(request.getReader(), UserCreatedDto.class);
+        User user = DataBase.findUserById(request.getParameter("userId"))
+                .orElseThrow(NotFoundUserException::new);
 
-        String userId = request.getParameter("userId");
-
-        User updateUser = new User(userId,
-                userDto.getPassword(),
-                userDto.getName(),
-                userDto.getEmail());
-
-        if (!UserSessionUtils.isSameUser(request.getSession(), updateUser)) {
+        if (!UserSessionUtils.isSameUser(request.getSession(), user)) {
             throw new LoginUserException();
         }
 
-        User user = UserSessionUtils.getUserFromSession(request.getSession());
+        User updateUser = new User(request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
+
         user.update(updateUser);
 
-        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        ModelAndView modelAndView = new ModelAndView(new RedirectView("/"));
         modelAndView.addObject("user", user);
         response.setStatus(HttpServletResponse.SC_OK);
         return modelAndView;
