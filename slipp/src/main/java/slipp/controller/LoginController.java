a7533bc2ffc4c4e764d582e94660959b93ea430f
김성훈 <seonghun127@gmail.com>
2019-10-14 09:45:04 +0900

    refactor: remove duplicated codes

--- slipp/src/main/java/slipp/controller/LoginController.java
+++ slipp/src/main/java/slipp/controller/LoginController.java
@@ -10,28 +10,28 @@ import slipp.domain.User;
 import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
 
     @RequestMapping(value = "/users/login", method = RequestMethod.POST)
-    public ModelAndView login(@RequestParam String userId, @RequestParam String password, HttpServletRequest request) { ;
+    public ModelAndView login(@RequestParam String userId, @RequestParam String password, HttpServletRequest request) {
+        ;
         User user = DataBase.findUserById(userId);
 
-        if (user == null) {
+        if (user == null || !user.matchPassword(password)) {
             request.setAttribute("loginFailed", true);
             return new ModelAndView(new JspView("/user/login.jsp"));
         }
+
         if (user.matchPassword(password)) {
             HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
             return new ModelAndView(new JspView("redirect:/"));
-        } else {
-            request.setAttribute("loginFailed", true);
-            return new ModelAndView(new JspView("/user/login.jsp"));
         }
+
+        throw new IllegalArgumentException("fail to login!");
     }
 
     @RequestMapping(value = "/users/logout", method = RequestMethod.GET)
