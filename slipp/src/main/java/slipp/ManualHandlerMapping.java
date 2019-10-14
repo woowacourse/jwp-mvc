bf6b8c57a516200071dea3cb81911b1729274cc0
Jo Jinsik <kunggom@gmail.com>
2019-10-14 17:00:55 +0900

    feat(UserController): 일부분 애노테이션 기반 MVC 적용

--- slipp/src/main/java/slipp/ManualHandlerMapping.java
+++ slipp/src/main/java/slipp/ManualHandlerMapping.java
@@ -2,10 +2,12 @@ package slipp;
 
 import nextstep.mvc.DispatcherServlet;
 import nextstep.mvc.asis.Controller;
-import nextstep.mvc.asis.ForwardController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import slipp.controller.*;
+import slipp.controller.CreateUserController;
+import slipp.controller.ProfileController;
+import slipp.controller.UpdateFormUserController;
+import slipp.controller.UpdateUserController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,12 +17,7 @@ class ManualHandlerMapping {
     private Map<String, Controller> mappings = new HashMap<>();
 
     void initialize() {
-        mappings.put("/users/form", new ForwardController("/user/form.jsp"));
-        mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
-        mappings.put("/users", new ListUserController());
-//        mappings.put("/users/login", new LoginController());
         mappings.put("/users/profile", new ProfileController());
-//        mappings.put("/users/logout", new LogoutController());
         mappings.put("/users/create", new CreateUserController());
         mappings.put("/users/updateForm", new UpdateFormUserController());
         mappings.put("/users/update", new UpdateUserController());
