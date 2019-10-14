124aacd5d0f21c1409c30c83629b4d0509cc9810
aiden <cnk9109@gmail.com>
2019-10-14 14:51:57 +0900

    feat: create JspViewReturnTypeResolver

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/view/returntyperesolver/JspViewReturnTypeResolver.java
@@ -0,0 +1,16 @@
+package nextstep.mvc.tobe.view.returntyperesolver;
+
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.view.ViewType;
+
+public class JspViewReturnTypeResolver implements ViewReturnTypeResolver {
+    @Override
+    public boolean support(Object handlerResult) {
+        return handlerResult instanceof String;
+    }
+
+    @Override
+    public ModelAndView resolve(Object handlerResult) {
+        return new ModelAndView(ViewType.JSP_VIEW, (String) handlerResult);
+    }
+}
