da897fa0108d85b2c95a372bfaf7e070772fbc4d
포비 <pobi@woowahan.com>
2019-10-15 13:31:33 +0900

    refactor: remove annotation config

--- nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -6,13 +6,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
