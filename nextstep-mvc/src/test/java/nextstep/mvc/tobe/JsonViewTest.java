6c403b0d898b99c20ce7d2fce1272489ab465bb3
YejiLee <32984517+yjll1019@users.noreply.github.com>
2019-10-16 21:05:02 +0900

    [MVC - Step 2] 티버 미션 제출합니다. (#79)
    * test: Jackson에 대한 학습 테스트 추가
    
    * feat: JsonViewTest 통과하도록 JsonView 구현
    
    * feat: UserAcceptanceTest가 통과하도록 UserController 추가
    
    * refactor: model 데이터의 개수에 따라 다르게 반환하도록 JsonView 수정
    
    * refactor: legacy mvc 제거
    
    * fix: user 목록을 출력하는 기능 수정
    
    * fix: UserAcceptanceTest 작동을 위한 버그 수정
    
    * fix: JsonViewTest 작동을 위한 버그 수정
    
    * refactor: 코드 컨벤션을 위한 수정
    
    * refactor: 모델이 비어있을 때 예외명, 예외 메시지 변경
    
    * test: 코드 컨벤션을 위한 수정
    
    * refactor: 회원 가입 시 User 생성 방법 변경
    
    * test: private 메서드 테스트 삭제
    
    * test: 패키지 이동
    
    * test: 컨트롤러 테스트 코드 추가
    
    * test: 코드 컨벤션을 위한 리팩토링
    
    * refactor: 새로운 매핑 방법을 적용하여 일부 컨트롤러 삭제 및 UserController 생성

--- nextstep-mvc/src/test/java/nextstep/mvc/tobe/JsonViewTest.java
+++ nextstep-mvc/src/test/java/nextstep/mvc/tobe/JsonViewTest.java
@@ -1,16 +1,20 @@
 package nextstep.mvc.tobe;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.utils.JsonUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -58,5 +62,39 @@ public class JsonViewTest {
 
         assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON_UTF8_VALUE);
         logger.debug("response body : {}", response.getContentAsString());
+    }
+
+    @Test
+    @DisplayName("전달하려는 모델의 개수가 1개일 때 value값 반환")
+    void return_value() {
+        JsonView jsonView = new JsonView();
+        Map<String, Object> model = new HashMap<>();
+        model.put("name", "포비");
+
+        assertThat(jsonView.convertJsonFromModel(model)).isEqualTo("포비");
+    }
+
+    @Test
+    @DisplayName("전달하려는 모델의 개수가 2개 이상일 때 map 반환")
+    void return_map() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JsonView jsonView = new JsonView();
+        Car expectedCar = new Car("Black", "Sonata");
+        Map<String, Object> expectedModels = new HashMap<>();
+
+        expectedModels.put("car", expectedCar);
+        expectedModels.put("name", "포비");
+
+        Object actual = jsonView.convertJsonFromModel(expectedModels);
+
+        Map<String, Object> actualModels = objectMapper.convertValue(
+                actual, new TypeReference<Map<String, Object>>() {
+                });
+
+        Car actualCar = objectMapper.convertValue(expectedModels.get("car"), Car.class);
+
+        assertThat(actualModels.get("name")).isEqualTo(expectedModels.get("name"));
+        assertThat(actualCar).isEqualTo(expectedCar);
     }
 }
