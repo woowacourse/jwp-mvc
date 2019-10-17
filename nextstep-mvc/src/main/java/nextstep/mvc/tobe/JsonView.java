9a40de7a1e56a4993f02a429f7347b7a4c22d573
Jo Jinsik <kunggom@gmail.com>
2019-10-17 17:48:39 +0900

    feat(JsonView): 주어진 JsonViewTest를 모두 통과

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/JsonView.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/JsonView.java
@@ -1,12 +1,20 @@
 package nextstep.mvc.tobe;
 
+import nextstep.utils.JsonUtils;
+import nextstep.web.support.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.util.Objects;
 
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        if (Objects.isNull(model) || model.isEmpty()) {
+            return;
+        }
+        response.getWriter().write(JsonUtils.toJson(model));
     }
 }
--- nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
+++ nextstep-mvc/src/main/java/nextstep/utils/JsonUtils.java
@@ -1,10 +1,13 @@
 package nextstep.utils;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.mvc.tobe.ObjectMapperException;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 public class JsonUtils {
     public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
@@ -18,5 +21,13 @@ public class JsonUtils {
         } catch (IOException e) {
             throw new ObjectMapperException(e);
         }
+    }
+
+    public static String toJson(final Map<String, ?> model) throws JsonProcessingException {
+        final ObjectMapper mapper = new ObjectMapper();
+        if (Objects.nonNull(model) && model.size() == 1) {
+            return mapper.writeValueAsString(model.values().toArray()[0]);
+        }
+        return mapper.writeValueAsString(model);
     }
 }
