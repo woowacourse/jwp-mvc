d2b6c3b9db5883e9fe93523f80976d6db46b5583
hyojaekim <hj00686@naver.com>
2019-10-19 17:36:53 +0900

    feat: RequestResolverAdapter 구현

---
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/RequestResolverAdapter.java
@@ -0,0 +1,24 @@
+package nextstep.mvc.tobe;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Parameter;
+
+public class RequestResolverAdapter implements ArgumentResolverAdapter {
+    private HttpServletRequest request;
+
+    public RequestResolverAdapter(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public boolean match(Parameter parameter) {
+        Class<?> parameterType = parameter.getType();
+
+        return parameterType.equals(HttpServletRequest.class);
+    }
+
+    @Override
+    public Object get(Parameter parameter) {
+        return request;
+    }
+}
