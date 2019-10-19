aaf07d177986e272329edc391770ac5ccad65f0e
hyojaekim <hj00686@naver.com>
2019-10-19 21:51:10 +0900

    test: ArgumentResolver 테스트 추가
    - ArgumentResolver 생성 테스트
    - 파라미터 타입 안맞을 때, 예외 발생하는 테스트

--- nextstep-mvc/src/test/java/nextstep/mvc/tobe/ArgumentResolverTest.java
+++ nextstep-mvc/src/test/java/nextstep/mvc/tobe/ArgumentResolverTest.java
@@ -1,43 +1,76 @@
 package nextstep.mvc.tobe;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
-import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ArgumentResolverTest {
     private static final Logger logger = LoggerFactory.getLogger(ArgumentResolverTest.class);
 
-    private ParameterNameDiscoverer nameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+    private static final String DEFAULT_USER_ID = "javajigi";
+
+    private Class clazz;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @BeforeEach
+    void setUp() {
+        this.clazz = TestUserController.class;
+        this.request = new MockHttpServletRequest();
+        this.response = new MockHttpServletResponse();
+
+        this.request.addParameter("userId", DEFAULT_USER_ID);
+    }
 
     @Test
-    void argumentResolver() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        String userId = "javajigi";
-        request.addParameter("userId", userId);
+    @DisplayName("정상적으로 ArgumentResolver를 생성한다.")
+    void createArgumentResolver() {
+        assertThat(new ArgumentResolver(request, response)).isNotNull();
+    }
 
-        Class clazz = TestUserController.class;
+    @Test
+    @DisplayName("메소드의 파라미터를 가져온다. (TestUserController -> requestParam)")
+    void requestParam() {
         Method method = getMethod("requestParam", clazz.getDeclaredMethods());
+        ArgumentResolver args = new ArgumentResolver(request, response);
 
-        ArgumentResolver args = new ArgumentResolver(request, new MockHttpServletResponse());
-        Object[] objects = args.resolve(method);
+        List<Object> parameters = Arrays.asList(args.resolve(method));
+        parameters = parameters.stream()
+                .map(Object::toString)
+                .collect(Collectors.toList());
 
-        boolean result = false;
-        for (Object object : objects) {
-            if (object.toString().equals(userId)) {
-                result = true;
-                break;
-            }
-        }
-        assertTrue(result);
+        assertTrue(parameters.contains(DEFAULT_USER_ID));
+    }
+
+    @Test
+    @DisplayName("메소드 파라미터 타입이 맞지 않아 예외가 발생한다. (TestUserController -> create_string)")
+    void create_string() {
+        Method method = getMethod("create_string", clazz.getDeclaredMethods());
+        ArgumentResolver args = new ArgumentResolver(request, response);
+
+        assertThrows(NotMatchParameterException.class, () -> args.resolve(method));
+    }
+
+    @Test
+    @DisplayName("메소드 파라미터 타입이 맞지 않아 예외가 발생한다. (TestUserController -> show_pathvariable)")
+    void show_pathvariable() {
+        Method method = getMethod("show_pathvariable", clazz.getDeclaredMethods());
+        ArgumentResolver args = new ArgumentResolver(request, response);
+
+        assertThrows(NotMatchParameterException.class, () -> args.resolve(method));
     }
 
     private Method getMethod(String name, Method[] methods) {
