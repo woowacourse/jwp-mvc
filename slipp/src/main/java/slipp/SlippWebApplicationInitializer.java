64b833ad4d0d30c0adfebe4e0fd0642d7050826a
Jo Jinsik <kunggom@gmail.com>
2019-10-21 21:38:06 +0900

    refactor: 요구사항 2 적용

--- slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
+++ slipp/src/main/java/slipp/SlippWebApplicationInitializer.java
@@ -10,13 +10,12 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class SlippWebApplicationInitializer  implements WebApplicationInitializer {
+public class SlippWebApplicationInitializer implements WebApplicationInitializer {
     private static final Logger log = LoggerFactory.getLogger(SlippWebApplicationInitializer.class);
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         final DispatcherServlet dispatcherServlet = new DispatcherServlet(
-                new ManualHandlerMappingAdapter(),
                 new AnnotationHandlerMapping("slipp.controller")
         );
 
--- slipp/src/main/java/slipp/controller/UserController.java
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -5,6 +5,7 @@ import nextstep.mvc.tobe.ModelAndView;
 import nextstep.mvc.tobe.RedirectView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
+import slipp.domain.User;
 import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,24 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController {
+    private static final ModelAndView REDIRECT_TO_ROOT = new ModelAndView(new RedirectView("/"));
+
+    @RequestMapping("/users")
+    public ModelAndView userList(final HttpServletRequest request, final HttpServletResponse response) {
+        if (!UserSessionUtils.isLogined(request.getSession())) {
+            return new ModelAndView(new RedirectView("/users/loginForm"));
+        }
+        request.setAttribute("users", DataBase.findAll());
+        return new ModelAndView(new JspView("/user/list"));
+    }
+
+    @RequestMapping("/users/create")
+    public ModelAndView createUser(final HttpServletRequest request, final HttpServletResponse response) {
+        final User user = makeUserFromParameter(request);
+        DataBase.addUser(user);
+        return REDIRECT_TO_ROOT;
+    }
+
     @RequestMapping("/users/form")
     public ModelAndView signUpForm(final HttpServletRequest request, final HttpServletResponse response) {
         return new ModelAndView(new JspView("/user/form"));
@@ -22,12 +41,46 @@ public class UserController {
         return new ModelAndView(new JspView("/user/login"));
     }
 
-    @RequestMapping("/users")
-    public ModelAndView userList(final HttpServletRequest request, final HttpServletResponse response) {
-        if (!UserSessionUtils.isLogined(request.getSession())) {
-            return new ModelAndView(new RedirectView("/users/loginForm"));
+    @RequestMapping("/users/profile")
+    public ModelAndView userProfile(final HttpServletRequest request, final HttpServletResponse response) {
+        final User user = findUserById(request);
+        request.setAttribute("user", user);
+        return new ModelAndView(new JspView("/user/profile"));
+    }
+
+    @RequestMapping("/users/updateForm")
+    public ModelAndView updateForm(final HttpServletRequest request, final HttpServletResponse response) {
+        final User user = findUserById(request);
+        checkIdForUpdate(request, user);
+        request.setAttribute("user", user);
+        return new ModelAndView(new JspView("/user/updateForm"));
+    }
+
+    @RequestMapping("/users/update")
+    public ModelAndView update(final HttpServletRequest request, final HttpServletResponse response) {
+        final User user = findUserById(request);
+        checkIdForUpdate(request, user);
+        final User newUser = makeUserFromParameter(request);
+        user.update(newUser);
+        DataBase.addUser(user);
+        return REDIRECT_TO_ROOT;
+    }
+
+    private User findUserById(final HttpServletRequest request) {
+        return DataBase.findUserById(request.getParameter("userId"));
+    }
+
+    private void checkIdForUpdate(final HttpServletRequest request, final User user) {
+        if (!UserSessionUtils.isSameUser(request.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
         }
-        request.setAttribute("users", DataBase.findAll());
-        return new ModelAndView(new JspView("/user/list"));
+    }
+
+    private User makeUserFromParameter(final HttpServletRequest request) {
+        return new User(
+                request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
     }
 }
