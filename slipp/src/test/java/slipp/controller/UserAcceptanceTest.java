5423b3e607bf4dc9009cfa9e49cde11afbff37ff
deocksoo <dusol92@snu.ac.kr>
2019-10-26 17:36:21 +0900

    refactor: UserAcceptanceTest를 분리합니다
    현재 user 생성, 조회, 수정에 대한 테스트가 모두 합쳐져있어서 문제가 생겼을 때 빠르게 파악하기
    어렵습니다. 이를 수정하였습니다.

--- slipp/src/test/java/slipp/controller/UserAcceptanceTest.java
+++ slipp/src/test/java/slipp/controller/UserAcceptanceTest.java
@@ -1,51 +1,64 @@
 package slipp.controller;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import slipp.domain.User;
 import slipp.dto.UserCreatedDto;
 import slipp.dto.UserUpdatedDto;
 import support.test.NsWebTestClient;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserAcceptanceTest {
-    private static final Logger logger = LoggerFactory.getLogger(UserAcceptanceTest.class);
+    private static NsWebTestClient client;
+    private static URI userLocation;
+    private static UserCreatedDto expected;
 
-    private NsWebTestClient client;
-
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         client = NsWebTestClient.of(8080);
+
+        expected = new UserCreatedDto(
+                "pobi",
+                "password",
+                "포비",
+                "pobi@nextstep.camp"
+        );
+        userLocation = client.createResource("/api/users", expected, UserCreatedDto.class);
     }
 
     @Test
-    @DisplayName("사용자 회원가입/조회/수정/삭제")
-    void crud() {
-        // 회원가입
-        UserCreatedDto expected =
-                new UserCreatedDto("pobi", "password", "포비", "pobi@nextstep.camp");
-        URI location = client.createResource("/api/users", expected, UserCreatedDto.class);
-        logger.debug("location : {}", location); // /api/users?userId=pobi 와 같은 형태로 반환
+    @DisplayName("사용자 회원 가입")
+    void createUser() throws URISyntaxException {
+        URI expected = new URI("/api/users?userId=pobi");
 
-        // 조회
-        User actual = client.getResource(location, User.class);
+        assertThat(userLocation).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("사용자 조회")
+    void findUer() {
+        User actual = client.getResource(userLocation, User.class);
+
         assertThat(actual.getUserId()).isEqualTo(expected.getUserId());
         assertThat(actual.getName()).isEqualTo(expected.getName());
         assertThat(actual.getEmail()).isEqualTo(expected.getEmail());
+    }
 
-        // 수정
-        UserUpdatedDto updateUser = new UserUpdatedDto("password2", "코난", "conan@nextstep.camp");
-        client.updateResource(location, updateUser, UserUpdatedDto.class);
+    @Test
+    @DisplayName("사용자 정보 수정")
+    void updateUser() {
+        UserUpdatedDto updatedUser = new UserUpdatedDto("password2", "코난", "conan@nextstep.camp");
 
-        actual = client.getResource(location, User.class);
-        assertThat(actual.getPassword()).isEqualTo(updateUser.getPassword());
-        assertThat(actual.getName()).isEqualTo(updateUser.getName());
-        assertThat(actual.getEmail()).isEqualTo(updateUser.getEmail());
+        client.updateResource(userLocation, updatedUser, UserUpdatedDto.class);
+
+        User actual = client.getResource(userLocation, User.class);
+        assertThat(actual.getPassword()).isEqualTo(updatedUser.getPassword());
+        assertThat(actual.getName()).isEqualTo(updatedUser.getName());
+        assertThat(actual.getEmail()).isEqualTo(updatedUser.getEmail());
     }
 }
