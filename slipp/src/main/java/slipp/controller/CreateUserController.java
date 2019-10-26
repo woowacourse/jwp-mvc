749dfab0e9de0accb8e2195a3fb19f77acf609e9
deocksoo <dusol92@snu.ac.kr>
2019-10-26 18:39:33 +0900

    refactor: CreateUserController 리펙토링
    1. objectMapper로 user 객체 만드는 부분 메서드 추출
    2. header의 location 필드 만드는 부분 추상화

--- slipp/src/main/java/slipp/controller/CreateUserController.java
+++ slipp/src/main/java/slipp/controller/CreateUserController.java
@@ -1,53 +1,53 @@
 package slipp.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nextstep.mvc.asis.Controller;
 import nextstep.mvc.tobe.ModelAndView;
-import nextstep.mvc.tobe.view.JsonView;
+import nextstep.mvc.tobe.view.EmptyView;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import slipp.controller.exception.UserCreationFailException;
 import slipp.domain.User;
 import slipp.support.db.DataBase;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 import static javax.servlet.http.HttpServletResponse.SC_CREATED;
 
 @nextstep.web.annotation.Controller
-public class CreateUserController implements Controller {
-    private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
-
-    @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) {
-        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
-                req.getParameter("email"));
-        log.debug("User : {}", user);
-
-        DataBase.addUser(user);
-        return "redirect:/";
-    }
+public class CreateUserController {
+    private static final Logger logger = LoggerFactory.getLogger(CreateUserController.class);
+    public static final String LOCATION = "location";
 
     @RequestMapping(value = "/api/users", method = RequestMethod.POST)
-    public ModelAndView execute2(HttpServletRequest req, HttpServletResponse resp) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        User user = null;
-        try {
-            user = objectMapper.readValue(req.getReader().readLine(), User.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        log.debug("User : {}", user);
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) {
+        User user = createUser(req);
 
         DataBase.addUser(user);
 
-        ModelAndView model = new ModelAndView(new JsonView());
         resp.setStatus(SC_CREATED);
-        resp.setHeader("location", "/api/users?userId=" + user.getUserId());
-        return model;
+        resp.setHeader(LOCATION, req.getRequestURI() + createQueryParams(user));
+        return new ModelAndView(new EmptyView());
+    }
+
+    private User createUser(HttpServletRequest req) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            String requestBody = req.getReader().readLine();
+            User created = objectMapper.readValue(requestBody, User.class);
+
+            logger.debug("User : {}", created);
+            return created;
+        } catch (IOException e) {
+            throw new UserCreationFailException(e.getCause());
+        }
+    }
+
+    private String createQueryParams(User user) {
+        return "?userId=" + user.getUserId();
     }
 }
