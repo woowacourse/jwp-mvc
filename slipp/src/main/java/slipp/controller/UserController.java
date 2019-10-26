0cc92ae1e8a37396f72d4081bd1c81992ad97119
deocksoo <dusol92@snu.ac.kr>
2019-10-27 02:42:10 +0900

    fix: User api가 아닌 기존의 User 관련 기능 누락한 실수 복원

---
+++ slipp/src/main/java/slipp/controller/UserController.java
@@ -0,0 +1,59 @@
+package slipp.controller;
+
+import nextstep.mvc.tobe.ModelAndView;
+import nextstep.mvc.tobe.view.JspView;
+import nextstep.mvc.tobe.view.RedirectView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import slipp.domain.User;
+import slipp.support.db.DataBase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/users/create", method = RequestMethod.POST)
+    public ModelAndView create(HttpServletRequest req, HttpServletResponse resp) {
+        User user = new User(
+                req.getParameter("userId"), req.getParameter("password"),
+                req.getParameter("name"), req.getParameter("email")
+        );
+        log.debug("User : {}", user);
+
+        DataBase.addUser(user);
+        return new ModelAndView(new RedirectView("/"));
+    }
+
+    @RequestMapping(value = "/users/profile", method = RequestMethod.GET)
+    public ModelAndView find(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            throw new NullPointerException("사용자를 찾을 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        return new ModelAndView(new JspView("/user/profile.jsp"));
+    }
+
+    @RequestMapping(value = "/users/update", method = RequestMethod.PUT)
+    public ModelAndView update(HttpServletRequest req, HttpServletResponse resp) {
+        User user = DataBase.findUserById(req.getParameter("userId"));
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+
+        User updateUser = new User(
+                req.getParameter("userId"), req.getParameter("password"),
+                req.getParameter("name"), req.getParameter("email")
+        );
+        log.debug("Update User : {}", updateUser);
+        user.update(updateUser);
+        return new ModelAndView(new RedirectView("redirect:/"));
+    }
+}
